81d6c56efab6ca32b13f7d2f1e083182a4850494
anyabot <52899201+anyabot@users.noreply.github.com>
2019-07-15 23:32:11 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,18 +3,30 @@ const fs = require("fs");
 
 const client = new Discord.Client();
 const config = require("./config.json");
+const commandObject = {
+  "info": "./commands/info.js"
+};
 // We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
 client.config = config;
 
-fs.readdir("./events/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    const event = require(`./events/${file}`);
-    let eventName = file.split(".")[0];
-    client.on(eventName, event.bind(null, client));
-  });
-});
+client.on("message", message => {
+  if (message.author.bot) return;
+  // This is where we'll put our code.
+  if (message.content.indexOf(config.prefix) !== 0) return;
+ 
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+ 
+  // Grab the command data from the client.commands Enmap
+  const cmd = commandObject[command];
 
+  // If that command doesn't exist, silently exit and do nothing
+  if (!cmd) return;
+
+  // Run the command
+  cmd.run(client, message, args);
+  }
+});
  
 
 // THIS  MUST  BE  THIS  WAY
