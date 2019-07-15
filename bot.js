e245a5ef80af1944fc2df1a810f60ecd96f50185
anyabot <52899201+anyabot@users.noreply.github.com>
2019-07-15 23:57:08 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,29 +3,46 @@ const fs = require("fs");
 
 const client = new Discord.Client();
 const config = require("./config.json");
-const commandObject = {
-  "info": "./commands/info.js"
-};
+
 // We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
 client.config = config;
 
 client.on("message", message => {
-  if (message.author.bot) return;
-  // This is where we'll put our code.
-  if (message.content.indexOf(config.prefix) !== 0) return;
+	if (message.author.bot) return;
+	// This is where we'll put our code.
+	if (message.content.indexOf(config.prefix) !== 0) return;
  
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
- 
-  // Grab the command data from the client.commands Enmap
-  const cmd = commandObject[command];
-
-  // If that command doesn't exist, silently exit and do nothing
-  if (!cmd) return;
-
-  // Run the command
-  cmd.run(client, message, args);
-  }
+	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+	const command = args.shift().toLowerCase();
+	
+	switch (command) {
+		case "ping" :
+			message.channel.send('Pong!');
+			break;
+		case "blah" :
+			message.channel.send('Meh.');
+			break;
+		case "info" :{
+			let name = args[0]
+			fs.readdir("./units/", (err, units) => {
+				if (err) return console.error(err);
+				var found = false
+				units.forEach(unit => {
+					unit.substring(0, unit.length - 1);
+					if (!unit = args[0]) return;
+					else {
+						found = true
+						const unit2 = require('./units/name);
+						let uname = unit2.name;
+						message.channel.send(uname);
+						break;
+					}
+				});
+				message.channel.send('Unit not found.');
+				break;
+			});	
+		}
+	}
 });
  
 
