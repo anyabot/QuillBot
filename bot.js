7224487d76d8dd6c9f4c7153f3ba906b418c659e
anyabot <52899201+anyabot@users.noreply.github.com>
2019-07-18 03:42:28 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 const client = new Discord.Client();
 const config = require("./config.json");
 const aff = require("./library/aff.js").aff
+const stat = require("./library/stat.js").stat
 
 // We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
 client.config = config;
