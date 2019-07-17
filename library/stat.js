60ab50a11f9dc712b8909834281d7b8a8198c254
anyabot <52899201+anyabot@users.noreply.github.com>
2019-07-18 03:59:04 +0900

    Update stat.js

--- library/stat.js
+++ library/stat.js
@@ -47,5 +47,53 @@ var stat = {
 		}
 		"icon": "https://vignette.wikia.nocookie.net/aigis/images/e/e9/Abel_Icon.png/revision/latest?cb=20180522205355"
 	},
+	"abel2": {
+		"name": "Abel",
+		"gender": "Male",
+		"rarity": "Gold",
+		"class": "Dragon Rider",
+		"stat": {
+			"class1": {
+				"name": "Dragon Rider (Lv1 → Lv 50)",
+				"hp": "1331 → 1870",
+				"atk": "406 → 520",
+				"def": "288 → 385",
+				"mr": "0",
+				"block": "1",
+				"range": "N/A",
+				"rangeskill": "250",
+				"rangesaw": "N/A",
+				"costmax": "29",
+				"costmin": "26"
+			},
+			"class2": {
+				"name": "Dragon Knight (Lv1 → Lv 60)",
+				"hp": "1879 → 2365",
+				"atk": "523 → 696",
+				"def": "397 → 493",
+				"mr": "0",
+				"block": "1",
+				"range": "N/A",
+				"rangeskill": "250",
+				"rangesaw": "N/A",
+				"costmax": "32",
+				"costmin": "29"
+			},
+			"class3": {
+				"name": "Dragon Lord (Lv1 → Lv 80)",
+				"hp": "2523 → 3788",
+				"atk": "718 → 936",
+				"def": "497 → 708",
+				"mr": "0",
+				"block": "1",
+				"range": "N/A",
+				"rangeskill": "250",
+				"rangesaw": "250",
+				"costmax": "37",
+				"costmin": "34"
+			}
+		}
+		"icon": "https://vignette.wikia.nocookie.net/aigis/images/e/e9/Abel_Icon.png/revision/latest?cb=20180522205355"
+	},
  }  
  exports.stat = stat  
