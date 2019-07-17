45b8460504f1a63e00b8b7a1839c027e86969007
anyabot <52899201+anyabot@users.noreply.github.com>
2019-07-18 03:03:30 +0900

    Add files via upload

---
+++ library/stat.js
@@ -0,0 +1,57 @@
+var aff = {	
+	"abel": {
+		"name": "Abel",
+		"aff": {
+			"a1": "HP+324",
+			"a2": "None",
+			"a3": "Not implemented"
+			},
+		"icon": "https://vignette.wikia.nocookie.net/aigis/images/e/e9/Abel_Icon.png/revision/latest?cb=20180522205355"
+		},
+	"ada": {
+		"name": "Ada",
+		"aff": {
+			"a1": "HP+432",
+			"a2": "ATK+144",
+			"a3": "Physical Attack Evasion+24%"
+			},
+		"icon": "https://vignette.wikia.nocookie.net/aigis/images/3/3a/Ada_Icon.png/revision/latest?cb=20180522205406"
+		},
+	"adelaide": {
+		"name": "Adelaide",
+		"aff": {
+			"a1": "HP+900",
+			"a2": "ATK+90",
+			"a3": "DEF+90"
+			},
+		"icon": "https://vignette.wikia.nocookie.net/aigis/images/8/8c/Adelaide_Icon.png/revision/latest?cb=20190328062604"
+		},
+	"adele": {
+		"name": "Adele",
+		"aff": {
+			"a1": "HP+360",
+			"a2": "ATK+144",
+			"a3": "ATK+72"
+			},
+		"icon": "https://vignette.wikia.nocookie.net/aigis/images/d/d7/Adele_Icon.png/revision/latest?cb=20180522205418"
+		}, 
+	"agnes": {
+		"name": "Agnes",
+		"aff": {
+			"a1": "ATK+172",
+			"a2": "DEF+72",
+			"a3": "Skill Duration+20%"
+			},
+		"icon": "https://vignette.wikia.nocookie.net/aigis/images/d/d7/Adele_Icon.png/revision/latest?cb=20180522205418"
+		}, 
+	"aida": {
+		"name": "Aida",
+		"aff": {
+			"a1": "ATK+108",
+			"a2": "DEF+108",
+			"a3": "ATK+72"
+			},
+		"icon": "https://vignette.wikia.nocookie.net/aigis/images/f/fa/Aida_Icon.png/revision/latest?cb=20180522205433"
+		}, 
+ }  
+ exports.aff = aff  
