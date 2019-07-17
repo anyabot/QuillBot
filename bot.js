5e7b11f3d785919b4002daae099eac4d984663bc
anyabot <52899201+anyabot@users.noreply.github.com>
2019-07-18 00:40:35 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 
 const client = new Discord.Client();
 const config = require("./config.json");
+const units = require("./library/units.js").units.
 
 // We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
 client.config = config;
@@ -22,25 +23,16 @@ client.on("message", message => {
 		case "blah" :
 			message.channel.send('Meh.');
 			break;
-		case "info" :{
-			let name = args[0]
-			fs.readdir("./units/", (err, units) => {
-				if (err) return console.error(err);
-				var found = false
-				units.forEach(unit => {
-					unit.substring(0, unit.length - 1);
-					if (!unit = args[0]) return;
-					else {
-						found = true
-						const unit2 = require('./units/name);
-						let uname = unit2.name;
-						message.channel.send(uname);
-						break;
-					}
-				});
-				message.channel.send('Unit not found.');
-				break;
-			});	
+		case "aff" :{
+			let name = args[0[;
+			if (units[name]) {
+				a1 = units[name].aff.11;
+				a2 = units[name].aff.12;
+				a3 = units[name].aff.15;
+				mes = "1st stat: " + a1 + "\n2nd stat: " + a2 + "\n150 affection stat: " +a3;
+				message.channel.send(mes);
+				}
+			};	
 		}
 	}
 });
