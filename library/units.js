1dc51b7d8d685370efe298fe128c62112971e270
anyabot <52899201+anyabot@users.noreply.github.com>
2019-07-18 02:07:12 +0900

    Update units.js

--- library/units.js
+++ library/units.js
@@ -4,47 +4,6 @@ var units = {
 		"gender": "Male",
 		"rarity": "Gold",
 		"class": "Dragon Rider",
-		"stat": {
-			"class1": {
-				"name": "Dragon Rider (Lv1 → Lv 50)",
-				"hp": "1331 → 1870",
-				"atk": "406 → 520",
-				"def": "288 → 385",
-				"mr": "0",
-				"block": "1",
-				"range": "N/A",
-				"rangeskill": "250",
-				"rangesaw": "N/A",
-				"costmax": "29",
-				"costmin": "26"
-			},
-			"class2": {
-				"name": "Dragon Knight (Lv1 → Lv 60)",
-				"hp": "1879 → 2365",
-				"atk": "523 → 696",
-				"def": "397 → 493",
-				"mr": "0",
-				"block": "1",
-				"range": "N/A",
-				"rangeskill": "250",
-				"rangesaw": "N/A",
-				"costmax": "32",
-				"costmin": "29"
-			},
-			"class3": {
-				"name": "Dragon Lord (Lv1 → Lv 80)",
-				"hp": "2523 → 3788",
-				"atk": "718 → 936",
-				"def": "497 → 708",
-				"mr": "0",
-				"block": "1",
-				"range": "N/A",
-				"rangeskill": "250",
-				"rangesaw": "250",
-				"costmax": "37",
-				"costmin": "34"
-			}
-		}
 		"aff": {
 			"a1": "HP+324",
 			"a2": "None",
