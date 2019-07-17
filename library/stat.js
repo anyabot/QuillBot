1b04949abe3cd77ed97b86efa72197bba9210f21
anyabot <52899201+anyabot@users.noreply.github.com>
2019-07-18 03:04:07 +0900

    Add files via upload

--- library/stat.js
+++ library/stat.js
@@ -1,57 +1,51 @@
-var aff = {	
+var stat = {	
 	"abel": {
 		"name": "Abel",
-		"aff": {
-			"a1": "HP+324",
-			"a2": "None",
-			"a3": "Not implemented"
+		"gender": "Male",
+		"rarity": "Gold",
+		"class": "Dragon Rider",
+		"stat": {
+			"class1": {
+				"name": "Dragon Rider (Lv1 → Lv 50)",
+				"hp": "1331 → 1870",
+				"atk": "406 → 520",
+				"def": "288 → 385",
+				"mr": "0",
+				"block": "1",
+				"range": "N/A",
+				"rangeskill": "250",
+				"rangesaw": "N/A",
+				"costmax": "29",
+				"costmin": "26"
 			},
+			"class2": {
+				"name": "Dragon Knight (Lv1 → Lv 60)",
+				"hp": "1879 → 2365",
+				"atk": "523 → 696",
+				"def": "397 → 493",
+				"mr": "0",
+				"block": "1",
+				"range": "N/A",
+				"rangeskill": "250",
+				"rangesaw": "N/A",
+				"costmax": "32",
+				"costmin": "29"
+			},
+			"class3": {
+				"name": "Dragon Lord (Lv1 → Lv 80)",
+				"hp": "2523 → 3788",
+				"atk": "718 → 936",
+				"def": "497 → 708",
+				"mr": "0",
+				"block": "1",
+				"range": "N/A",
+				"rangeskill": "250",
+				"rangesaw": "250",
+				"costmax": "37",
+				"costmin": "34"
+			}
+		}
 		"icon": "https://vignette.wikia.nocookie.net/aigis/images/e/e9/Abel_Icon.png/revision/latest?cb=20180522205355"
-		},
-	"ada": {
-		"name": "Ada",
-		"aff": {
-			"a1": "HP+432",
-			"a2": "ATK+144",
-			"a3": "Physical Attack Evasion+24%"
-			},
-		"icon": "https://vignette.wikia.nocookie.net/aigis/images/3/3a/Ada_Icon.png/revision/latest?cb=20180522205406"
-		},
-	"adelaide": {
-		"name": "Adelaide",
-		"aff": {
-			"a1": "HP+900",
-			"a2": "ATK+90",
-			"a3": "DEF+90"
-			},
-		"icon": "https://vignette.wikia.nocookie.net/aigis/images/8/8c/Adelaide_Icon.png/revision/latest?cb=20190328062604"
-		},
-	"adele": {
-		"name": "Adele",
-		"aff": {
-			"a1": "HP+360",
-			"a2": "ATK+144",
-			"a3": "ATK+72"
-			},
-		"icon": "https://vignette.wikia.nocookie.net/aigis/images/d/d7/Adele_Icon.png/revision/latest?cb=20180522205418"
-		}, 
-	"agnes": {
-		"name": "Agnes",
-		"aff": {
-			"a1": "ATK+172",
-			"a2": "DEF+72",
-			"a3": "Skill Duration+20%"
-			},
-		"icon": "https://vignette.wikia.nocookie.net/aigis/images/d/d7/Adele_Icon.png/revision/latest?cb=20180522205418"
-		}, 
-	"aida": {
-		"name": "Aida",
-		"aff": {
-			"a1": "ATK+108",
-			"a2": "DEF+108",
-			"a3": "ATK+72"
-			},
-		"icon": "https://vignette.wikia.nocookie.net/aigis/images/f/fa/Aida_Icon.png/revision/latest?cb=20180522205433"
-		}, 
+	},
  }  
- exports.aff = aff  
+ exports.stat = stat  
