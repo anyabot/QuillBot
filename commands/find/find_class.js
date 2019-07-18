17bdb0ac87a3130eefa490ff7c16206f7827e2c9
anyabot <52899201+anyabot@users.noreply.github.com>
2019-07-18 23:54:18 +0900

    Update find_class.js

--- commands/find/find_class.js
+++ commands/find/find_class.js
@@ -23,35 +23,35 @@ class FindClass extends commando.Command {
         	var name = class.toLowerCase();
 		if (unitclass[name]) {
 			let classname = unitclass[name];
-			if (classname["base"]{
+			if (classname["base"]) {
 				const embed = new Discord.RichEmbed()
 				embed.setTitle(classname.base.name)
 				embed.addField("Description", classname.base.des, true)
 				embed.addField("Note", classname.base.note, true)
 				message.channel.send({embed});
 			}
-			if (classname["cc"]{
+			if (classname["cc"]) {
 				const embed2 = new Discord.RichEmbed()
 				embed2.setTitle(classname.cc.name)
 				embed2.addField("Description", classname.cc.des, true)
 				embed2.addField("Note", classname.cc.note, true)
 				message.channel.send({embed2});
 			}
-			if (classname["aw"]{
+			if (classname["aw"]){
 				const embed3 = new Discord.RichEmbed()
 				embed3.setTitle(classname.aw.name)
 				embed3.addField("Description", classname.aw.des, true)
 				embed3.addField("Note", classname.aw.note, true)
 				message.channel.send({embed3});
 			}
-			if (classname["aw2v1"]{
+			if (classname["aw2v1"]) {
 				const embed4 = new Discord.RichEmbed()
 				embed4.setTitle(classname.aw2v1.name)
 				embed4.addField("Description", classname.aw2v1.des, true)
 				embed4.addField("Note", classname.aw2v1.note, true)
 				message.channel.send({embed4});
 			}
-			if (classname["aw2v2"]{
+			if (classname["aw2v2"]) {
 				const embed5 = new Discord.RichEmbed()
 				embed5.setTitle(classname.aw2v2.name)
 				embed5.addField("Description", classname.aw2v2.des, true)
