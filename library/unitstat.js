0d427fea80f6a05cd54c178c407bfa58e063018a
anyabot <52899201+anyabot@users.noreply.github.com>
2019-07-18 23:19:38 +0900

    Update unitstat.js

--- library/unitstat.js
+++ library/unitstat.js
@@ -17,33 +17,8 @@ var unitstat = {
 				"rangesaw": "N/A",
 				"costmax": "29",
 				"costmin": "26"
-			},
-			"class2": {
-				"name": "Dragon Knight (Lv1 → Lv 60)",
-				"hp": "1879 → 2365",
-				"atk": "523 → 696",
-				"def": "397 → 493",
-				"mr": "0",
-				"block": "1",
-				"range": "N/A",
-				"rangeskill": "250",
-				"rangesaw": "N/A",
-				"costmax": "32",
-				"costmin": "29"
-			},
-			"class3": {
-				"name": "Dragon Lord (Lv1 → Lv 80)",
-				"hp": "2523 → 3788",
-				"atk": "718 → 936",
-				"def": "497 → 708",
-				"mr": "0",
-				"block": "1",
-				"range": "N/A",
-				"rangeskill": "250",
-				"rangesaw": "250",
-				"costmax": "37",
-				"costmin": "34"
 			}
+			
 		}
 		"icon": "https://vignette.wikia.nocookie.net/aigis/images/e/e9/Abel_Icon.png/revision/latest?cb=20180522205355"
 	},
@@ -65,32 +40,6 @@ var unitstat = {
 				"rangesaw": "N/A",
 				"costmax": "29",
 				"costmin": "26"
-			},
-			"class2": {
-				"name": "Dragon Knight (Lv1 → Lv 60)",
-				"hp": "1879 → 2365",
-				"atk": "523 → 696",
-				"def": "397 → 493",
-				"mr": "0",
-				"block": "1",
-				"range": "N/A",
-				"rangeskill": "250",
-				"rangesaw": "N/A",
-				"costmax": "32",
-				"costmin": "29"
-			},
-			"class3": {
-				"name": "Dragon Lord (Lv1 → Lv 80)",
-				"hp": "2523 → 3788",
-				"atk": "718 → 936",
-				"def": "497 → 708",
-				"mr": "0",
-				"block": "1",
-				"range": "N/A",
-				"rangeskill": "250",
-				"rangesaw": "250",
-				"costmax": "37",
-				"costmin": "34"
 			}
 		}
 		"icon": "https://vignette.wikia.nocookie.net/aigis/images/e/e9/Abel_Icon.png/revision/latest?cb=20180522205355"
