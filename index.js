699875164d20f5257d98c2dd9f53274ebf9763d4
anyabot <52899201+anyabot@users.noreply.github.com>
2019-07-18 22:27:10 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 const { commando } = require('discord.js-commando');
-const path = require('path');
 const client = new CommandoClient({
     commandPrefix: '!',
     owner: '371341098854907939',
     disableEveryone: true
 });
 
-client.registry
-    .registerDefaultTypes()
-    .registerGroups([
-        ['find', 'Find']
-    ])
-    .registerDefaultGroups()
-    .registerDefaultCommands()
-    .registerCommandsIn(path.join(__dirname, 'commands'));
+    client.registry.registerDefaultTypes()
+    client.registry.registerGroups('find', 'Find')
+    client.registry.registerDefaultGroups()
+    client.registry.registerDefaultCommands()
+    client.registry.registerCommandsIn(path.join(__dirname, 'commands'));
 
 client.on('ready', () => {
     console.log('Logged in!');
