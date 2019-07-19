1aea9bc8b68400765b6cf206af3d6125eda9fa43
anyabot <52899201+anyabot@users.noreply.github.com>
2019-07-19 17:49:42 +0900

    Update find_stat.js

--- commands/find/find_stat.js
+++ commands/find/find_stat.js
@@ -25,32 +25,17 @@ class FindStat extends commando.Command {
                 const $ = cheerio.load(html);
                 var text;
 		var output;
-                        
+                var img;
+		var img2;
 		var output = $('.c3.numbers').first().text();
-		if(output) {
-                	var lv99v1 = lv99line(output);
-                	output = $('.c3 ').first().text();
-                	var lv1v1 = lv1line(output);
-			var img = ($('.c3 td:first-child div a').attr('href'));
-			const embed3 = new Discord.RichEmbed()
-			embed3.setTitle(lv1v1[0] + "(" + lv1v1[1] + " → " + lv99v1[0] + ")")
-			embed3.setThumbnail(img)
-			embed3.addField("HP", lv1v1[2] + " → " + lv99v1[1], true)
-			embed3.addField("ATK", lv1v1[3] + " → " + lv99v1[2], true)
-			embed3.addField("DEF", lv1v1[4] + " → " + lv99v1[3], true)
-			embed3.addField("Range", lv99v1[4], true)
-			embed3.addField("MR", lv1v1[5], true)
-			embed3.addField("Block", lv1v1[6], true)
-			embed3.addField("Max Cost", lv1v1[7], true)
-			embed3.addField("Min Cost", lv1v1[8], true)
-			message.channel.send({embed3});
-		}
+		
                 output = $('.c4.numbers').first().text();
 		if(output) {
                 	var lv99v1 = lv99line(output);
                 	output = $('.c4 ').first().text();
                 	var lv1v1 = lv1line(output);
-			
+			img2 = ($('.c4 td:first-child div a').attr('href'));
+			if (img2) {img = img2}
 			const embed4 = new Discord.RichEmbed()
 			embed4.setTitle(lv1v1[0] + "(" + lv1v1[1] + " → " + lv99v1[0] + ")")
 			embed4.setThumbnail(img)
@@ -64,25 +49,7 @@ class FindStat extends commando.Command {
 			embed4.addField("Min Cost", lv1v1[8], true)
 			message.channel.send({embed4});
 		}
-                output = $('.c5.numbers').first().text();
-		if(output) {
-                	var lv99v1 = lv99line(output);
-                	output = $('.c5 ').first().text();
-                	var lv1v1 = lv1line(output);
-			
-			const embed5 = new Discord.RichEmbed()
-			embed5.setTitle(lv1v1[0] + "(" + lv1v1[1] + " → " + lv99v1[0] + ")")
-			embed5.setThumbnail(img)
-			embed5.addField("HP", lv1v1[2] + " → " + lv99v1[1], true)
-			embed5.addField("ATK", lv1v1[3] + " → " + lv99v1[2], true)
-			embed5.addField("DEF", lv1v1[4] + " → " + lv99v1[3], true)
-			embed5.addField("Range", lv99v1[4], true)
-			embed5.addField("MR", lv1v1[5], true)
-			embed5.addField("Block", lv1v1[6], true)
-			embed5.addField("Max Cost", lv1v1[7], true)
-			embed5.addField("Min Cost", lv1v1[8], true)
-			message.channel.send({embed5});
-		}
+                
             }
         });
     }
