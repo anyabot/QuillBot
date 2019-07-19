1d9c81d8c021fde8a135a60d614328561962a7fa
anyabot <52899201+anyabot@users.noreply.github.com>
2019-07-19 17:57:42 +0900

    Update find_stat.js

--- commands/find/find_stat.js
+++ commands/find/find_stat.js
@@ -25,28 +25,27 @@ class FindStat extends commando.Command {
                 const $ = cheerio.load(html);
                 var text;
 		var output;
-                var img;
-		var img2;
-		
+                        
+
                 output = $('.c4.numbers').first().text();
-		
+		if(output) {
                 	var lv99v1 = lv99line(output);
                 	output = $('.c4 ').first().text();
                 	var lv1v1 = lv1line(output);
-			img = ($('.c4 td:first-child div a').attr('href'));
-			const embed4 = new Discord.RichEmbed()
-			embed4.setTitle(lv1v1[0] + "(" + lv1v1[1] + " → " + lv99v1[0] + ")")
-			embed4.setThumbnail(img)
-			embed4.addField("HP", lv1v1[2] + " → " + lv99v1[1], true)
-			embed4.addField("ATK", lv1v1[3] + " → " + lv99v1[2], true)
-			embed4.addField("DEF", lv1v1[4] + " → " + lv99v1[3], true)
-			embed4.addField("Range", lv99v1[4], true)
-			embed4.addField("MR", lv1v1[5], true)
-			embed4.addField("Block", lv1v1[6], true)
-			embed4.addField("Max Cost", lv1v1[7], true)
-			embed4.addField("Min Cost", lv1v1[8], true)
-			message.channel.send({embed4});
-		
+			var img = ($('.c4 td:first-child div a').attr('href'));
+			const embed = new Discord.RichEmbed()
+			.setTitle(lv1v1[0] + "(" + lv1v1[1] + " → " + lv99v1[0] + ")")
+			.setThumbnail(img)
+			.addField("HP", lv1v1[2] + " → " + lv99v1[1], true)
+			.addField("ATK", lv1v1[3] + " → " + lv99v1[2], true)
+			.addField("DEF", lv1v1[4] + " → " + lv99v1[3], true)
+			.addField("Range", lv99v1[4], true)
+			.addField("MR", lv1v1[5], true)
+			.addField("Block", lv1v1[6], true)
+			.addField("Max Cost", lv1v1[7], true)
+			.addField("Min Cost", lv1v1[8], true)
+			message.channel.send({embed});
+		}
                 
             }
         });
