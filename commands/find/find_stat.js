70c6f8c3c2b58ea6776a918038f820e0f609fe94
anyabot <52899201+anyabot@users.noreply.github.com>
2019-07-19 17:31:50 +0900

    Update find_stat.js

--- commands/find/find_stat.js
+++ commands/find/find_stat.js
@@ -24,26 +24,28 @@ class FindStat extends commando.Command {
             if (!err) {
                 const $ = cheerio.load(html);
                 var text;
+		var output;
                         
 
-                var output = $('.c4.numbers').first().text();
-                var lv99v1 = lv99line(output);
-                output = $('.c4 ').first().text();
-                var lv1v1 = lv1line(output);
-		var img = ($('.c4 td:first-child div a').attr('href'));
-		const embed = new Discord.RichEmbed()
-		.setTitle(lv1v1[0] + "(" + lv1v1[1] + " → " + lv99v1[0] + ")")
-		.setThumbnail(img)
-		.addField("HP", lv1v1[2] + " → " + lv99v1[1], true)
-		.addField("ATK", lv1v1[3] + " → " + lv99v1[2], true)
-		.addField("DEF", lv1v1[4] + " → " + lv99v1[3], true)
-		.addField("Range", lv99v1[4], true)
-		.addField("MR", lv1v1[5], true)
-		.addField("Block", lv1v1[6], true)
-		.addField("Max Cost", lv1v1[7], true)
-		.addField("Min Cost", lv1v1[8], true)
-		message.channel.send({embed});
-				
+                output = $('.c4.numbers').first().text();
+		if(output) {
+                	var lv99v1 = lv99line(output);
+                	output = $('.c4 ').first().text();
+                	var lv1v1 = lv1line(output);
+			var img = ($('.c4 td:first-child div a').attr('href'));
+			const embed = new Discord.RichEmbed()
+			.setTitle(lv1v1[0] + "(" + lv1v1[1] + " → " + lv99v1[0] + ")")
+			.setThumbnail(img)
+			.addField("HP", lv1v1[2] + " → " + lv99v1[1], true)
+			.addField("ATK", lv1v1[3] + " → " + lv99v1[2], true)
+			.addField("DEF", lv1v1[4] + " → " + lv99v1[3], true)
+			.addField("Range", lv99v1[4], true)
+			.addField("MR", lv1v1[5], true)
+			.addField("Block", lv1v1[6], true)
+			.addField("Max Cost", lv1v1[7], true)
+			.addField("Min Cost", lv1v1[8], true)
+			message.channel.send({embed});
+		}
                 
             }
         });
