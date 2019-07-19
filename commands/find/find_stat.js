4087c069f28af21a66edd8d20e8304d78382b2b2
anyabot <52899201+anyabot@users.noreply.github.com>
2019-07-19 18:02:19 +0900

    Update find_stat.js

--- commands/find/find_stat.js
+++ commands/find/find_stat.js
@@ -29,11 +29,30 @@ class FindStat extends commando.Command {
 
                 output = $('.c4.numbers').first().text();
 		if(output) {
-                	var lv99v1 = lv99line(output);
+                	let lv99v1 = lv99line(output);
                 	output = $('.c4 ').first().text();
-                	var lv1v1 = lv1line(output);
-			var img = ($('.c4 td:first-child div a').attr('href'));
-			const embed = new Discord.RichEmbed()
+                	let lv1v1 = lv1line(output);
+			let img = ($('.c4 td:first-child div a').attr('href'));
+			let embed = new Discord.RichEmbed()
+			.setTitle(lv1v1[0] + "(" + lv1v1[1] + " → " + lv99v1[0] + ")")
+			.setThumbnail(img)
+			.addField("HP", lv1v1[2] + " → " + lv99v1[1], true)
+			.addField("ATK", lv1v1[3] + " → " + lv99v1[2], true)
+			.addField("DEF", lv1v1[4] + " → " + lv99v1[3], true)
+			.addField("Range", lv99v1[4], true)
+			.addField("MR", lv1v1[5], true)
+			.addField("Block", lv1v1[6], true)
+			.addField("Max Cost", lv1v1[7], true)
+			.addField("Min Cost", lv1v1[8], true)
+			message.channel.send({embed});
+		}
+		output = $('.c5.numbers').first().text();
+		if(output) {
+                	let lv99v1 = lv99line(output);
+                	output = $('.c5 ').first().text();
+                	let lv1v1 = lv1line(output);
+			let img = ($('.c5 td:first-child div a').attr('href'));
+			let embed = new Discord.RichEmbed()
 			.setTitle(lv1v1[0] + "(" + lv1v1[1] + " → " + lv99v1[0] + ")")
 			.setThumbnail(img)
 			.addField("HP", lv1v1[2] + " → " + lv99v1[1], true)
