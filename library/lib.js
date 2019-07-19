1e9b10bc0179ef987c057299cfd72f016e0f2caf
anyabot <52899201+anyabot@users.noreply.github.com>
2019-07-19 16:28:10 +0900

    Create lib.js

---
+++ library/lib.js
@@ -0,0 +1,29 @@
+var name = {
+    'Snekloli' : 'Orochihime',
+    'Shampoo' : 'Jin-Guang Sheng-Pu',
+    'Gelbro' : 'Gellius',
+    'Shoka' : 'Shokatsuryou',
+    'White Fluff' : 'Kyuubi',
+    'Gold Fluff' : 'Kayou',
+    'Chocorino' : 'Rino (Valentine\'s)',
+    'Chainloli' : 'Salus',
+    'Hammerloli' : 'Teeny',
+    'Swordloli' : 'Rischelte',
+    'Waterloli' : 'Cenote',
+    'Darkloli' : 'Clarice',
+    'Catloli' : 'Minette',
+    'Danchou' : 'Dark Knight',
+    'Dark' : 'D\'Arc',
+    'Cucumber' : 'Kyuubi',
+    'Ate' : 'Kibahime',
+    'Kure' : 'Kanon',
+    'Hk' : 'Marie',
+    'Pomf' : 'Tenma',
+    'Anon' : 'Remii',
+    'Will' : 'Mikoto',
+    'Electric' : 'Rion',
+    'Mmm' : 'Quill',
+    'Banana' : 'Nanaly'
+}
+
+exports.name = name;
