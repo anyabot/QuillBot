f87c4bd0dd8b296133549bc6bb64051b97e4ecf3
anyabot <52899201+anyabot@users.noreply.github.com>
2019-07-20 11:07:09 +0900

    Update find_stat.js

--- commands/find/find_stat.js
+++ commands/find/find_stat.js
@@ -19,6 +19,7 @@ class FindStat extends commando.Command {
     async run(message, input) {
         var unit = functions.toTitleCase(input);
         if (name[unit]) unit = name[unit];
+	message.channel.send(unit);
         var link = "https://aigis.fandom.com/wiki/" + unit;
 
         request(link, function (err, resp, html) {
