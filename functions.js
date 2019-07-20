abfd5af0d0d48a8ab3e88b26a95e7258593cf607
anyabot <52899201+anyabot@users.noreply.github.com>
2019-07-20 11:03:14 +0900

    Update functions.js

--- functions.js
+++ functions.js
@@ -4,15 +4,4 @@ exports.toTitleCase = function toTitleCase(str) {
         return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
     });
 }
-// capitalize Aw, Aw2v1, Aw2v2, Aa
-exports.toAW = function toAW(str) {
-    return str.replace(/((Aw)|(Aa))(\w+)?/g, function (txt) {
-        return txt.charAt(0).toUpperCase() + txt.charAt(1).toUpperCase() + txt.substr(2).toLowerCase();
-    });
-}
-// delete Aw, Aw2v1, Aw2v2, Aa
-exports.delAW = function delAW(str) {
-    return str.replace(/(( Aw)|( Aa))(\w+)?/g, function (txt) {
-        return "";
-    });
-}
+
