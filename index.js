72242ee314175c7cd137534e3c7202bfeed187bf
anyabot <52899201+anyabot@users.noreply.github.com>
2019-07-20 16:06:45 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const commando = require('discord.js-commando');
-const bot = new commando.CommandoClient({
+const { CommandoClient } = require('discord.js-commando');
+const bot = new CommandoClient({
     commandPrefix: '~',
     owner: '371341098854907939',
     disableEveryone: true
