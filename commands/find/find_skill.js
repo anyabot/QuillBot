77b04d53cde41a4ba53fcae2b71382351590b398
anyabot <52899201+anyabot@users.noreply.github.com>
2019-07-20 22:59:48 +0900

    Update find_skill.js

--- commands/find/find_skill.js
+++ commands/find/find_skill.js
@@ -34,7 +34,7 @@ class FindSkill extends commando.Command {
 					output = $(elem).first().text();
 					let ar = te(output);
 					if (ar[0] === 'Unit'){
-						message.channel.send($(elem).length);
+						message.channel.send($(elem).first().nextAll().length);
 					}
 				})
 		if (check) {
