c0b402272c02fe4d3641a13c948c351024c25d0d
anyabot <52899201+anyabot@users.noreply.github.com>
2019-07-22 02:24:01 +0900

    Update find_ability.js

--- commands/find/find_ability.js
+++ commands/find/find_ability.js
@@ -131,6 +131,39 @@ if (bronze) {
               embed.addField("Description", des);
               if (note != '' && note != null) {embed.addField("Notes", note)};
 			        pages.push(embed2)
+		    embed = pages[0];
+		embed.setFooter('Page ' + page + ' of ' + pages.length);
+		message.channel.send(embed).then(msg => {
+
+		msg.react('⬅').then( r => {
+        msg.react('➡')
+
+        // Filters
+        const backwardsFilter = (reaction, user) => reaction.emoji.name === '⬅' && !user.bot;
+        const forwardsFilter = (reaction, user) => reaction.emoji.name === '➡' && !user.bot;
+
+        const backwards = msg.createReactionCollector(backwardsFilter, {timer: 6000});
+        const forwards = msg.createReactionCollector(forwardsFilter, {timer: 6000});
+
+        backwards.on('collect', r => {
+		r.remove(r.users.filter(u => !u.bot).first());
+        	if (page === 1) return;
+         	page--;
+            	embed = pages[page-1];
+            	embed.setFooter('Page ' + page + ' of ' + pages.length);
+            	msg.edit(embed)
+        })
+
+        forwards.on('collect', r => {
+		r.remove(r.users.filter(u => !u.bot).first());
+            	if (page === pages.length) return;
+            	page++;
+            	embed = pages[page-1];
+            	embed.setFooter('Page ' + page + ' of ' + pages.length);
+            	msg.edit(embed)
+        })
+    })
+})
             }
           })
         }
