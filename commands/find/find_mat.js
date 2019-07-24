695a6b30e2f95c4670e350f4aa5cab2426cef9c7
anyabot <52899201+anyabot@users.noreply.github.com>
2019-07-24 11:31:47 +0900

    Update find_mat.js

--- commands/find/find_mat.js
+++ commands/find/find_mat.js
@@ -8,33 +8,55 @@ var name = require('../../library/lib.js').name;
 require('@gouch/to-title-case')
 
 var cc1 = {
-	"/wiki/Asar": "Soldier (Iron)",
-	"/wiki/Leeanne": "Heavy Armor (Iron)",
-	"/wiki/Valerie": "Mage (Iron)",
-	"/wiki/Soma" :"Archer (Iron)",
-	"/wiki/Mortimer" :"Bandit (Iron)",
-	"/wiki/Saizou" :"Ninja (Iron)"
+	"/wiki/Asar": "Iron Soldier",
+	"/wiki/Leeanne": "Iron Heavy Armor",
+	"/wiki/Valerie": "Iron Mage",
+	"/wiki/Soma" :"Iron Archer",
+	"/wiki/Mortimer" :"Iron Bandit",
+	"/wiki/Saizou" :"Iron Ninja"
 }
 var cc2 = {
-	"/wiki/Asar": "Soldier (Silver)",
-	"/wiki/Leeanne": "Heavy Armor (Silver)",
-	"/wiki/Valerie": "Mage (Silver)",
-	"/wiki/Niel" :"Angel (Silver)",
-	"/wiki/Soma" :"Archer (Silver)",
-	"/wiki/Mortimer" :"Bandit (Silver)",
-	"/wiki/Elaine" :"Valkyrie (Silver)",
-	"/wiki/Alissa" :"Healer (Silver)",
-	"/wiki/Saizou" :"Ninja (Silver)",
-	"/wiki/Calliope" :"Witch (Silver)",
-	"/wiki/Fudou" :"Bishop (Silver)",
-	"/wiki/Cecily" :"Rogue (Silver)",
-	"/wiki/Sanosuke" :"Samurai (Silver)",
-	"/wiki/Percis" :"Gunner (Silver)",
-	"/wiki/Ricardo" :"Magic Fencer (Silver)",
-	"/wiki/Khuri" :"Vampire Hunter (Silver)",
-	"/wiki/Giovanni" :"Pirate (Silver)",
-	"/wiki/Dan" :"Monk (Silver)",
-	"/wiki/Ertel" :"Priest Warrior (Silver)"
+	"/wiki/Asar": "Silver Soldier",
+	"/wiki/Leeanne": "Silver Heavy Armor",
+	"/wiki/Valerie": "Silver Mage",
+	"/wiki/Niel" :"Silver Angel",
+	"/wiki/Soma" :"Silver Archer",
+	"/wiki/Mortimer" :"Silver Bandit",
+	"/wiki/Elaine" :"Silver Valkyrie",
+	"/wiki/Alissa" :"Silver Healer",
+	"/wiki/Saizou" :"Silver Ninja",
+	"/wiki/Calliope" :"Silver Witch",
+	"/wiki/Fudou" :"Silver Bishop",
+	"/wiki/Cecily" :"Silver Rogue",
+	"/wiki/Sanosuke" :"Silver Samurai",
+	"/wiki/Percis" :"Silver Gunner",
+	"/wiki/Ricardo" :"Silver Magic Fencer",
+	"/wiki/Khuri" :"Silver Vampire Hunter",
+	"/wiki/Giovanni" :"Silver Pirate",
+	"/wiki/Dan" :"Silver Monk",
+	"/wiki/Ertel" :"Silver Priest Warrior"
+}
+var aw1 = {
+	"/wiki/Asar": "Gold Soldier \nOR \nCC Silver Soldier: \n1 x Iron Soldier \n1 x Iron Heavy Armor \n2 x Silver Soldiers \n1 x Spirit of Silver (Cyrille) \nOR \nSpirit Queen (Gladys)",
+	"/wiki/Leeanne": "Gold Heavy Armor \nOR \nCC Silver Heavy Armor: \n1 x Iron Soldier \n1 x Iron Heavy Armor \n2 x Silver Heavy Armors \n1 x Spirit of Silver (Cyrille) \nOR \nSpirit Queen (Gladys)",
+	"/wiki/Valerie": "Gold Mage \nOR \nCC Silver Mage: \n1 x Iron Soldier \n1 x Iron Mage \n2 x Silver Mages \n1 x Spirit of Silver (Cyrille) \nOR \nSpirit Queen (Gladys)",
+	"/wiki/Niel" :"Gold Angel \nOR \nCC Silver Angel: \n1 x Iron Soldier \n1 x Iron Heavy Armor \n2 x Silver Angels \n1 x Spirit of Silver (Cyrille) \nOR \nSpirit Queen (Gladys)",
+	"/wiki/Daniela" :"Gold Archer \nOR \nCC Silver Bandit: \n2 x Iron Archers \n2 x Silver Archers \n1 x Spirit of Silver (Cyrille) \nOR \nSpirit Queen (Gladys)",
+	"/wiki/Eunice" :"Gold Bandit \nOR \nCC Silver Bandit: \n1 x Iron Soldier \n1 x Iron Bandit \n2 x Silver Bandits \n1 x Spirit of Silver (Cyrille) \nOR \nSpirit Queen (Gladys)",
+	"/wiki/Elaine" :"Gold Valkyrie \nOR \nCC Silver Valkyrie: \n1 x Iron Soldier \n1 x Iron Heavy Armor \n2 x Silver Valkyries \n1 x Spirit of Silver (Cyrille) \nOR \nSpirit Queen (Gladys)",
+	"/wiki/Alissa" :"Gold Healer \nOR \nCC Silver Healer: \n1 x Iron Archer \n1 x Iron Mage \n2 x Silver Healers \n1 x Spirit of Silver (Cyrille) \nOR \nSpirit Queen (Gladys)",
+	"/wiki/Saizou" :"Gold Ninja \nOR \nCC Silver Ninja: \n1 x Iron Soldier \n1 x Iron Bandit \n2 x Silver Ninjas \n1 x Spirit of Silver (Cyrille) \nOR \nSpirit Queen (Gladys)",
+	"/wiki/Calliope" :"Gold Witch \nOR \nCC Silver Witch: \n1 x Iron Archer \n1 x Iron Mage, \n2 x Silver Witches \n1 x Spirit of Silver (Cyrille) \nOR \nSpirit Queen (Gladys)",
+	"/wiki/Fudou" :"Gold Bishop \nOR \nCC Silver Bishop: \n2 x Iron Mages \n1 x Silver Healers \n1 x Spirit of Silver (Cyrille) \nOR \nSpirit Queen (Gladys) \n1 x Silver Bishop",
+	"/wiki/Cecily" :"Gold Rogue \nOR \nCC Silver Rouge: \n1 x Iron Soldier \n1 x Iron Archer, \n2 x Silver Rouges \n1 x Spirit of Silver (Cyrille) \nOR \nSpirit Queen (Gladys)",
+	"/wiki/Sanosuke" :"Gold Samurai \nOR \nCC Silver Samurai: \n1 x Iron Soldier \n1 x Iron Mage \n2 x Silver Samurai \n1 x Spirit of Silver (Cyrille) \nOR \nSpirit Queen (Gladys)",
+	"/wiki/Percis" :"Gold Gunner \nOR \nCC Silver Gunner: \n1 x Iron Soldier \n1 x Iron Archer, \n2 x Silver Gunners \n1 x Spirit of Silver (Cyrille) \nOR \nSpirit Queen (Gladys)",
+	"/wiki/Ricardo" :"Gold Magic Fencer \nOR \nCC Silver Magic Fencer: \n1 x Iron Soldier \n1 x Iron Mage \n2 x Silver Magic Fencers \n1 x Spirit of Silver (Cyrille) \nOR \nSpirit Queen (Gladys)",
+	"/wiki/Khuri" :"Gold Vampire Hunter \nOR \nCC Vampire Hunter: \n2 x Iron Archers \n2 x Silver Vampire Hunters \n1 x Spirit of Silver (Cyrille) \nOR \nSpirit Queen (Gladys)",
+	"/wiki/Giovanni" :"Gold Pirate \nOR \nCC Silver Pirate: \n1 x Iron Archer \n1 x Iron Bandit, \n2 x Silver Pirates \n1 x Spirit of Silver (Cyrille) \nOR \nSpirit Queen (Gladys)",
+	"/wiki/Dan" :"Gold Monk \nOR \nCC Silver Monk: \n1 x Iron Soldier \n1 x Iron Archer, \n2 x Silver Monks \n1 x Spirit of Silver (Cyrille) \nOR \nSpirit Queen (Gladys)",
+	"/wiki/Ertel" :"Gold Priest Warrior \nOR \nCC Priest Warrior: \n1 x Iron Soldier \n1 x Iron Heavy Armor \n2 x Silver Priest Warriors \n1 x Spirit of Silver (Cyrille) \nOR \nSpirit Queen (Gladys)",
+	"/wiki/Gadoras" :"Gold Dragon Soldier \nOR CC Silver Dragon Soldier: \n1 x Iron Soldier \n1 x Iron Bandit \n1 x Silver Heavy Armor \n1 x Spirit of Silver (Cyrille) \nOR \nSpirit Queen (Gladys) \n1 x Silver Dragon Soldier"
 }
 
 class FindMat extends commando.Command {
@@ -127,6 +149,32 @@ request(link, function(err, resp, html) {
 					if (plat) {embedcc.addField("Fairy", "Spirit of Platinum (Celia) \nOR \nSpirit Queen (Gladys)", true)}
 					if (black) {embedcc.addField("Fairy", "Spirit of Black (Florika) \nOR \nSpirit Queen (Gladys)", true)}
 					message.channel.send(embedcc)
+					if (aw) {
+						link2 = "https://aigis.fandom.com/wiki/Awakening/" + awname
+						request(link2, function(err, resp, html) {
+							if (!err) {
+								$2 = cheerio.load(html)
+								mat1 = $2('.gcstyle.bgwhite tr:nth-child(2) td:nth-child(2) table tbody tr td div a').attr('href')
+								mat2 = $2('.gcstyle.bgwhite tr:nth-child(3) td:nth-child(2) table tbody tr td div a').attr('href')
+								mat3 = $2('.gcstyle.bgwhite tr:nth-child(4) td:nth-child(2) table tbody tr td div a').attr('href')
+								let embedaw = new Discord.RichEmbed();
+								let orbs = $2('.gcstyle.bgwhite tr:nth-child(5) td:nth-child(3)').text()
+								let parts = mat1.split('&')
+								let len = parts.length
+								parts[len-1] = parts[len-1].slice(0,-1)
+								for (var i = 0; i < len; i++) {
+									parts[i] = parts[i].slice(5)
+								}
+								embedaw.setTitle("AW Materials")
+								embedaw.setThumbnail(awimg)
+								embedaw.addField("Material 1", aw1[mat1], true)
+								embedaw.addField("Material 2", aw1[mat2], true)
+								embedaw.addField("Material 3", aw1[mat3], true)
+								embedaw.addField("Fairy", "Spirit of Awakening (Victoire)", true)
+								message.channel.send(embedaw)
+							}
+						})
+					}
 				}	
 			})
 		}
