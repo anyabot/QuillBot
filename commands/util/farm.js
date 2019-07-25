ea1b8e181cb4f9bc60054b9c2919b6a1b2c9d325
anyabot <52899201+anyabot@users.noreply.github.com>
2019-07-25 18:51:31 +0900

    Update farm.js

--- commands/util/farm.js
+++ commands/util/farm.js
@@ -42,7 +42,7 @@ request(link, function(err, resp, html) {
 					let cha = na($(elem).find('tr').eq(j).children().eq(3).text())
 					let sta = na($(elem).find('tr').eq(j).children().eq(4).text())
 					let sname = na($(elem).find('tr').eq(j).children().eq(5).find('a').attr('title'))
-					let sname2 = na($(elem).find('tr').eq(j).children().eq(5).find('a').attr('title'))
+					let sname2 = na($(elem).find('tr').eq(j).children().eq(6).find('a').attr('title'))
 					if (j%8 == 2 && j != 2) {
 						pages.push(embed)
 						embed = new Discord.RichEmbed()
