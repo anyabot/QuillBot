879118475e99ef713fb36c5be9631680e04ef099
anyabot <52899201+anyabot@users.noreply.github.com>
2019-07-26 16:53:56 +0900

    Create suf.js

---
+++ library/suf.js
@@ -0,0 +1,14 @@
+var suffix = {
+    '7sins' : '【7Sins】',
+    'Christmas' : '(Christmas)',
+    'Dress' : '(Dress)',
+    'Yukata' : '(Yukata)',
+    'Swimsuit' : '(Swimsuit)',
+    'Valentine\'s' : '(Valentine\'s)',
+    'Valentine' : '(Valentine's)',
+    'School' : '(School)',
+    'Bride' : '(Bride)',
+    'Halloween' : '(Halloween)',
+}
+
+exports.suffix = suffix;
