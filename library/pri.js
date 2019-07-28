40c24ae0676f738bab64055eb0a2944e814ef164
anyabot <52899201+anyabot@users.noreply.github.com>
2019-07-28 23:07:16 +0900

    Create pri.js

---
+++ library/pri.js
@@ -0,0 +1,14 @@
+var name = {
+    'Pika' : 'Keraunos',
+    'Tesla' : 'Keraunos',
+    'Electromagnetic Gandhi': 'Keraunos',
+    'Aw' : 'Sacred Gear',
+    'Sacred Equipment' : 'Sacred Gear',
+    'Sacred' : 'Sacred Gear',
+    'Child' : 'Childhood',
+    'Bird' : 'Sacred Gear',
+    'Wind' : 'Wind Guardian',
+    'Heroic' : 'Einherjar',
+}
+
+exports.name = name;
