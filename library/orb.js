6769a8ac3ef21d575faa2551b67c0ba3c90329c2
anyabot <52899201+anyabot@users.noreply.github.com>
2019-07-29 23:13:16 +0900

    Create orb.js

---
+++ library/orb.js
@@ -0,0 +1,11 @@
+var name = {
+    'monday' : 'mon',
+    'tuesday' : 'tue',
+    'wednesday': 'wed',
+    'thursday' : 'thu',
+    'friday' : 'fri',
+    'saturday' : 'sat',
+    'sunday' : 'sun',
+}
+
+exports.name = name;
