9e9fecb110150c517413c35f43b1479cf3ac4e84
anyabot <52899201+anyabot@users.noreply.github.com>
2019-07-29 10:14:50 +0900

    Update lib.js

--- library/lib.js
+++ library/lib.js
@@ -44,7 +44,22 @@ var name = {
     '<:lapis:586229170854690829>': 'Lapis',
     '<:toto:379074268245393428>': 'Totono',
     '<:belinda:540272828940025856>': 'Belinda',
-    '<:conrad_close:509740993722056724>': 'Conrad'
+    '<:conrad_close:509740993722056724>': 'Conrad',
+    '<:trash:509476668335718403>': 'Trash',
+    '<:this_again:590376075058479141>': 'Faust',
+    '<:sabein:589682136416780308>': 'Sabein',
+    '<:rita:230607647659786240>': 'Rita',
+    '<:rinne_wan:589543928735465472>': 'Rinne',
+    '<:nana:230607642614038528>': 'Nanaly',
+    '<:kt:324195941647253535>': 'Katie',
+    '<:kibabliss:589846322392203293>': 'Kibahime',
+    '<:kanonstare:589846793278455810>': 'Kanon',
+    '<:elvastare:589544336199254018>': 'Elva',
+    '<:chibishu:589846185083142145>': 'Shu-Xian',
+    '<:chibikiba:589846219015192635>': 'Kibahime',
+    '<:chibi_iris:230607661903773696>': 'Iris',
+    '<:chibi_cam:230607655654260737>': 'Camilla',
+    '<:calliope:398400399050276875>': 'Calliope'
 }
 
 exports.name = name;
