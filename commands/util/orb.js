813a9d2ea53432089f99afc4eab885487a8cba69
anyabot <52899201+anyabot@users.noreply.github.com>
2019-07-29 22:44:25 +0900

    Update orb.js

--- commands/util/orb.js
+++ commands/util/orb.js
@@ -34,26 +34,44 @@ class FindOrb extends commando.Command {
 			else {tday = " "}
 			line = printf("|%-10s | %-20s | %-20s | %-20s |\n|           |——————————————————————|——————————————————————|——————————————————————|\n|%-10s | %-20s | %-20s | %-20s |"," Monday", "Sniper", "Dark Knight", "Priest", tday, "Rearguard Strategist", "Heavy Gunner", " ")
 			ms = ms + line + "\n|————————————————————————————————————————————————————————————————————————————————|"
+			ms = ms + "```"
+			message.channel.send(ms)
+			var ms = "```|————————————————————————————————————————————————————————————————————————————————|\n"
 			if (date == "Tue") {tday = "(Today)"}
 			else {tday = " "}
 			line = printf("|%-10s | %-20s | %-20s | %-20s |\n|           |——————————————————————|——————————————————————|——————————————————————|\n|%-10s | %-20s | %-20s | %-20s |"," Tuesday", "Unicorn Knight", "Pegasus Knight", "Master Monk", tday, "Berserker", "Feng Shui Master", " ")
 			ms = ms + line + "\n|————————————————————————————————————————————————————————————————————————————————|"
+			ms = ms + "```"
+			message.channel.send(ms)
+			ms = "```|————————————————————————————————————————————————————————————————————————————————|\n"
 			if (date == "Wed") {tday = "(Today)"}
 			else {tday = " "}
 			line = printf("|%-10s | %-20s | %-20s | %-20s |\n|           |——————————————————————|——————————————————————|——————————————————————|\n|%-10s | %-20s | %-20s | %-20s |"," Wednesday", "Soldier Chief", "Assassin", "Captain", tday, "High Shaman", "High Bishop", " ")
 			ms = ms + line + "\n|————————————————————————————————————————————————————————————————————————————————|"
+			ms = ms + "```"
+			message.channel.send(ms)
+			ms = "```|————————————————————————————————————————————————————————————————————————————————|\n"
 			if (date == "Thu") {tday = "(Today)"}
 			else {tday = " "}
 			line = printf("|%-10s | %-20s | %-20s | %-20s |\n|           |——————————————————————|——————————————————————|——————————————————————|\n|%-10s | %-20s | %-20s | %-20s |"," Thursday", "Battle Master", "Samurai Master", "Rune Fencer", tday, "Vampire Killer", "Sailor Chief", " ")
 			ms = ms + line + "\n|————————————————————————————————————————————————————————————————————————————————|"
+			ms = ms + "```"
+			message.channel.send(ms)
+			ms = "```|————————————————————————————————————————————————————————————————————————————————|\n"
 			if (date == "Fri") {tday = "(Today)"}
 			else {tday = " "}
 			line = printf("|%-10s | %-20s | %-20s | %-20s |\n|           |——————————————————————|——————————————————————|——————————————————————|\n|%-10s | %-20s | %-20s | %-20s |"," Friday", "Lord Witch", "Warlock", "Vanguard Strategist", tday, "Ninja Master", "Arch Angel", " ")
 			ms = ms + line + "\n|————————————————————————————————————————————————————————————————————————————————|"
+			ms = ms + "```"
+			message.channel.send(ms)
+			ms = "```|————————————————————————————————————————————————————————————————————————————————|\n"
 			if (date == "Sat") {tday = "(Today)"}
 			else {tday = " "}
 			line = printf("|%-10s | %-20s | %-20s | %-20s |\n|           |——————————————————————|——————————————————————|——————————————————————|\n|%-10s | %-20s | %-20s | %-20s |"," Saturday", "Battle Mage", "High Ranger", "Top Dancer", tday, "Dragon Knight", "Priest Warrior Leader", " ")
 			ms = ms + line + "\n|————————————————————————————————————————————————————————————————————————————————|"
+			ms = ms + "```"
+			message.channel.send(ms)
+			ms = "```|————————————————————————————————————————————————————————————————————————————————|\n"
 			if (date == "Sun") {tday = "(Today)"}
 			else {tday = " "}
 			line = printf("|%-10s | %-20s | %-20s | %-20s |\n|           |——————————————————————|——————————————————————|——————————————————————|\n|%-10s | %-20s | %-20s | %-20s |"," Sunday", "High Alchemist", "Witch Doctor", "Machinist", tday, "Master Thief", "Bowrider Chief", " ")
