b31bc224245f42da535f72b6c15c38c9d528d1d4
anyabot <52899201+anyabot@users.noreply.github.com>
2019-07-31 18:36:28 +0900

    Update roll.js

--- commands/ran/roll.js
+++ commands/ran/roll.js
@@ -43,25 +43,25 @@ class RanRoll extends commando.Command {
 			if (rar < 4) {
 				var ind = random.int(1, size_dict(black))
 				var unit = black[ind]
-				embed.setDescription("You rolled " + unit + " (Black)")
+				embed.setDescription("<@" + message.author.id + "> You rolled " + unit + " (6*)")
 				embed.setColor([95, 64, 0])
 			}
 			else if (rar < 14) {
 				var ind = random.int(1, size_dict(plat))
 				var unit = plat[ind]
-				embed.setDescription("You rolled " + unit + " (Platinum)")
+				embed.setDescription("<@" + message.author.id + "> You rolled " + unit + " (5*)")
 				embed.setColor('GREEN')
 			}
 			else if (rar < 64) {
 				var ind = random.int(1, size_dict(gold))
 				var unit = gold[ind]
-				embed.setDescription("You rolled " + unit + " (Gold)")
+				embed.setDescription("<@" + message.author.id + "> You rolled " + unit + " (4*)")
 				embed.setColor('GOLD')
 			}
 			else {
 				var ind = random.int(1, size_dict(sil))
 				var unit = sil[ind]
-				embed.setDescription("You rolled " + unit + " (Silver)")
+				embed.setDescription("<@" + message.author.id + "> You rolled " + unit + " (3*)")
 				embed.setColor('WHITE')
 			}
 			var img
@@ -81,25 +81,25 @@ class RanRoll extends commando.Command {
 			if (rar < 4) {
 				var ind = random.int(1, size_dict(iblack))
 				var unit = iblack[ind]
-				embed.setDescription("You rolled " + unit + " (Black)")
+				embed.setDescription("<@" + message.author.id + "> You rolled " + unit + " (6*)")
 				embed.setColor([95, 64, 0])
 			}
 			else if (rar < 14) {
 				var ind = random.int(1, size_dict(iplat))
 				var unit = iplat[ind]
-				embed.setDescription("You rolled " + unit + " (Platinum)")
+				embed.setDescription("<@" + message.author.id + "> You rolled " + unit + " (5*)")
 				embed.setColor('GREEN')
 			}
 			else if (rar < 64) {
 				var ind = random.int(1, size_dict(igold))
 				var unit = igold[ind]
-				embed.setDescription("You rolled " + unit + " (Gold)")
+				embed.setDescription("<@" + message.author.id + "> You rolled " + unit + " (4*)")
 				embed.setColor('GOLD')
 			}
 			else {
 				var ind = random.int(1, size_dict(isil))
 				var unit = isil[ind]
-				embed.setDescription("You rolled " + unit + " (Silver)")
+				embed.setDescription("<@" + message.author.id + "> You rolled " + unit + " (3*)")
 				embed.setColor('WHITE')
 			}
 			var img
@@ -109,7 +109,6 @@ class RanRoll extends commando.Command {
 					const $ = cheerio.load(html);
 					img = $('.fullImageLink a').attr('href')
 					embed.setImage(img)
-					message.channel.send("<@" + message.author.id + ">")
 					message.channel.send(embed)
 				}
 			})
@@ -120,25 +119,25 @@ class RanRoll extends commando.Command {
 			if (rar < 4) {
 				var ind = random.int(1, size_dict(pugblack))
 				var unit = pugblack[ind]
-				embed.setDescription("You rolled " + unit + " (Black)")
+				embed.setDescription("<@" + message.author.id + "> You rolled " + unit + " (6*)")
 				embed.setColor([95, 64, 0])
 			}
 			else if (rar < 14) {
 				var ind = random.int(1, size_dict(pugplat))
 				var unit = pugplat[ind]
-				embed.setDescription("You rolled " + unit + " (Platinum)")
+				embed.setDescription("<@" + message.author.id + "> You rolled " + unit + " (5*)")
 				embed.setColor('GREEN')
 			}
 			else if (rar < 64) {
 				var ind = random.int(1, size_dict(gold))
 				var unit = gold[ind]
-				embed.setDescription("You rolled " + unit + " (Gold)")
+				embed.setDescription("<@" + message.author.id + "> You rolled " + unit + " (4*)")
 				embed.setColor('GOLD')
 			}
 			else {
 				var ind = random.int(1, size_dict(sil))
 				var unit = sil[ind]
-				embed.setDescription("You rolled " + unit + " (Silver)")
+				embed.setDescription("<@" + message.author.id + "> You rolled " + unit + " (3*)")
 				embed.setColor('WHITE')
 			}
 			var img
