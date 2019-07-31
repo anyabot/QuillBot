3749fe37ad48b0b26c20439ad1acc004eff8aebd
anyabot <52899201+anyabot@users.noreply.github.com>
2019-07-31 17:53:48 +0900

    Create iblack.js

---
+++ library/iblack.js
@@ -0,0 +1,13 @@
+var iblack = {
+    1 : 'Angeline',
+    2 : 'Diana',
+    3 : 'Elmira',
+    4 : 'Kyuubi',
+    5 : 'Sieglinde',
+	6 : 'Wurm',
+	7 : 'Mephisto',
+	8 : 'Aurora',
+	9 : 'Eleonora'
+}
+
+exports.iblack = iblack;
