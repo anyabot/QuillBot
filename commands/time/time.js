ab9bcf4171f38a5f323e6015bdc543e96f96b1e5
anyabot <52899201+anyabot@users.noreply.github.com>
2019-08-01 10:28:31 +0900

    Update time.js

--- commands/time/time.js
+++ commands/time/time.js
@@ -41,44 +41,44 @@ class TTime extends commando.Command {
 		var m2 = moment(year + " " +  month + " " + day, 'YYYY MMM DD').add(1, 'days')
 		var mm2 = m2.toString()
 		var dif1 = m2.diff(m)
-		var diff1 = humanizeDuration(dif1, { round: true })
+		var diff1 = humanizeDuration(dif1, { units: ['d', 'h', 'm'] , round: true })
 		var m3 = moment(year + " " +  month + " " + day +" 04:00:00", 'YYYY MMM DD HH:mm:ss')
 		if (m3.isBefore(m)) {m3 = m3.add(1, 'days')}
 		var dif2 = m3.diff(m)
-		var diff2 = humanizeDuration(dif2, { round: true })
+		var diff2 = humanizeDuration(dif2, { units: ['d', 'h', 'm'] , round: true })
 		var mai = false;
 		var ms = moment(year + " " +  month + " " + day +" 11:00:00", 'YYYY MMM DD HH:mm:ss')
 		var me = moment(year + " " +  month + " " + day +" 15:00:00", 'YYYY MMM DD HH:mm:ss')
 		if (date == "Thu") {
 			if (m.isBefore(ms)) {
 				var dif3 = ms.diff(m)
-				var diff3 = humanizeDuration(dif3, { round: true })
+				var diff3 = humanizeDuration(dif3, { units: ['d', 'h', 'm'] , round: true })
 			}
 			else if (m.isBefore(me)) {
 				mai = true;
 				var dif3 = me.diff(m)
-				var diff3 = humanizeDuration(dif3, { round: true })
+				var diff3 = humanizeDuration(dif3, { units: ['d', 'h', 'm'] , round: true })
 			  }
 			else {
 				ms = ms.add(7, 'days')
 				var dif3 = ms.diff(m)
-				var diff3 = humanizeDuration(dif3, { round: true })
+				var diff3 = humanizeDuration(dif3, { units: ['d', 'h', 'm'] , round: true })
 			}
 		}
 		else {
 			ms = ms.add(daynum[date], 'days')
 			var dif3 = ms.diff(m)
-			var diff3 = humanizeDuration(dif3, { round: true })
+			var diff3 = humanizeDuration(dif3, { units: ['d', 'h', 'm'] , round: true })
 		}
 		var m4 = moment(year + " " +  month + " " + "01" +" 04:00:00", 'YYYY MMM DD HH:mm:ss')
 		if (m.isBefore(m4)) {
 			var dif4 = m4.diff(m)
-			var diff4 = humanizeDuration(dif4, { round: true })
+			var diff4 = humanizeDuration(dif4, { units: ['d', 'h', 'm'] , round: true })
 		}
 		else {
 			m4 = m4.add(1, 'months')
 			var dif4 = m4.diff(m)
-			var diff4 = humanizeDuration(dif4, { round: true })
+			var diff4 = humanizeDuration(dif4, { units: ['d', 'h', 'm'] , round: true })
 		}
 		var mm4 = m4.format('ddd MMM DD YYYY HH mm ss')
 		var words4 = mm4.split(' ')
@@ -92,12 +92,12 @@ class TTime extends commando.Command {
 		}
 		if (m.isBefore(m5)) {
 			var dif5 = m5.diff(m)
-			var diff5 = humanizeDuration(dif5, { round: true })
+			var diff5 = humanizeDuration(dif5, { units: ['d', 'h', 'm'] , round: true })
 		}
 		else {
 			m5 = m5.add(1, 'months')
 			var dif5 = m5.diff(m)
-			var diff5 = humanizeDuration(dif5, { round: true })
+			var diff5 = humanizeDuration(dif5, { units: ['d', 'h', 'm'] , round: true })
 		}
 		var embed = new Discord.RichEmbed()
 		embed.setColor('RANDOM')
