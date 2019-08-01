0bd03c8132a9c6a85a263492176f0592293d98ac
anyabot <52899201+anyabot@users.noreply.github.com>
2019-08-01 10:37:07 +0900

    Update pugplat.js

--- library/pugplat.js
+++ library/pugplat.js
@@ -1,9 +1,9 @@
 var pugplat = {
-    1 : 'Liddy',
-    2 : 'Lythtis',
-    3 : 'Clarice',
-    4 : 'Shinato',
-    5 : 'Longji Gongzhu'
+    1 : 'Sherry',
+    2 : 'Erenia',
+    3 : 'Racua',
+    4 : 'Blanche',
+    5 : 'Lumedia'
 }
 
 exports.pugplat = pugplat;
