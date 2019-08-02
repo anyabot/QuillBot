7c180be2c330d7739e23570277bc81edbfd45e41
anyabot <52899201+anyabot@users.noreply.github.com>
2019-08-02 17:16:25 +0900

    Create test.js

---
+++ commands/ran/test.js
@@ -0,0 +1,39 @@
+const commando = require('discord.js-commando');
+const Discord = require('discord.js');
+var request = require('request');
+var cheerio = require('cheerio');
+var he = require('he');
+const random = require('random')
+var urlencode = require('urlencode');
+const Keyv = require('keyv');
+require('@keyv/mongo')
+
+
+
+class RanRoll extends commando.Command {
+    constructor(client) {
+        super(client, {
+            	name: 'credit',
+            	group: 'ran',
+            	memberName: 'credit',
+            	description: 'gacha stimulator',
+		examples: ['&roll'],
+		args: [{
+		    key: 'text',
+			prompt: 'What pool do you want to pull from?',
+		    type: 'string',
+		default: "default"
+		}]
+        });
+    }
+
+    async run(message, { text }) {
+		  const sc = new Keyv(MONGODB_URI, { namespace: 'users' });
+      var usc = await sc.get(message.author.id)
+      if (usc == undefined) {usc = 0}
+      usc = usc +3;
+      message.channel.send(usc)
+				
+	}
+}
+module.exports = RanRoll;
