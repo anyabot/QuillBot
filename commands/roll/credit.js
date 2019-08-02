abe2100a8266bddbc57d6fd7585e8dcdee5a0a64
anyabot <52899201+anyabot@users.noreply.github.com>
2019-08-02 21:12:51 +0900

    Create credit.js

---
+++ commands/roll/credit.js
@@ -0,0 +1,36 @@
+const commando = require('discord.js-commando');
+const Discord = require('discord.js');
+const random = require('random')
+const Keyv = require('keyv');
+require('@keyv/mysql')
+
+
+
+class RanRoll extends commando.Command {
+    constructor(client) {
+        super(client, {
+            	name: 'credit',
+            	group: 'roll',
+              aliases: ['sc'],
+            	memberName: 'credit',
+            	description: 'get free sc for the gacha stimulator once every 30 mins',
+		throttling: {
+		usages: 1,
+		duration: 1800
+	},
+		examples: ['&credit']
+        });
+    }
+
+    async run(message, input) {
+		  const sc = new Keyv(process.env.CLEARDB_DATABASE_URL, { namespace: 'sc' });
+      var usc = await sc.get(message.author.id)
+      if (usc == undefined) {usc = 0}
+	    var ge = random.int(5, 10)
+      usc = usc + ge;
+	sc.set(message.author.id, usc)
+      message.reply("You got " + ge + " SC\nYou curently have " + usc + " SC")
+				
+	}
+}
+module.exports = RanRoll;
