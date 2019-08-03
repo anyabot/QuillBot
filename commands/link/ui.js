fb17703680fa7767be812fdfeb2bac8db78a855a
anyabot <52899201+anyabot@users.noreply.github.com>
2019-08-04 00:13:08 +0900

    Create ui.js

---
+++ commands/link/ui.js
@@ -0,0 +1,20 @@
+const commando = require('discord.js-commando');
+const Discord = require('discord.js');
+
+class UtilBuff extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'uiguide',
+		aliases: ['ui'],
+            group: 'link',
+            memberName: 'uiguide',
+            description: 'link to wiki ui guide',
+		examples: ['&classbuff'],
+        });
+    }
+
+    async run(message, input) {
+		message.channel.send('https://aigis.fandom.com/wiki/UI_Guide')
+    }
+}
+module.exports = UtilBuff;
