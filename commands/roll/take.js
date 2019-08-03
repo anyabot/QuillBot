b66d77ba528bd2e748293c3363421d2a87c7c3b6
anyabot <52899201+anyabot@users.noreply.github.com>
2019-08-03 12:11:49 +0900

    Create take.js

---
+++ commands/roll/take.js
@@ -0,0 +1,57 @@
+const commando = require('discord.js-commando');
+const Discord = require('discord.js');
+const random = require('random')
+const Keyv = require('keyv');
+require('@keyv/mysql')
+require('@keyv/mongo')
+
+
+
+class RanRoll extends commando.Command {
+    constructor(client) {
+        super(client, {
+            	name: 'take',
+            	group: 'roll',
+            	memberName: 'take',
+            	description: 'take a unit from your previous roll to the barrack (max capacity 100 units)',
+		examples: ['&take']
+        });
+    }
+
+    async run(message, input) {
+		 const lastroll = new Keyv(process.env.MONGODB_URI, { namespace: 'lastroll' });
+	    lastroll.on('error', err => console.error('Keyv connection error:', err));
+		const barrack = new Keyv(process.env.MONGODB_URI, { namespace: 'barrack' });
+	    barrack.on('error', err => console.error('Keyv connection error:', err));
+		var ulastroll = await lastroll.get(message.author.id)
+		if (ulastroll == undefined) {ulastroll = []}
+		var ubarrack = await barrack.get(message.author.id)
+		if (ubarrack == undefined) {ubarrack = []}
+	    if (ulastroll == [] || ulastroll == null) {
+			message.reply("You have no unit to take")
+		} 
+		else if (ubarrack.length > 99) {message.reply("Your barrack is full"}
+		else {
+			var mes = "Units from your last roll:"
+			for (var i = 0; i < ulastroll.length; i++) {
+				mes = mes + "\n" + i + ". " + ulastroll[i]
+			}
+			const collector = new Discord.MessageCollector(message.channel, msg => msg.author.id === message.author.id, { time: 6000 });
+			mes = mes + "\nWhich unit do you want to take? (Input the index number)"
+			message.reply(mes)
+			collector.on('collect', msg => {
+				const ind = msg.content
+                if (0 < ind && ind < (lastroll.length + 1) && !isNaN(ind)) {
+					ind = ind - 1;
+					ubarrack.push(ulastroll[ind])
+					collector.stop()
+				}
+				else {
+					message.reply("Wrong Input")
+					collector.stop()
+				}
+            })
+		}		
+	}
+}
+module.exports = RanRoll;
