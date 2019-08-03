a054ed061872aa3f2958d7b1e17b0cdc80d2ab5f
anyabot <52899201+anyabot@users.noreply.github.com>
2019-08-04 00:16:38 +0900

    Create implementationlog.js

---
+++ commands/link/implementationlog.js
@@ -0,0 +1,20 @@
+const commando = require('discord.js-commando');
+const Discord = require('discord.js');
+
+class UtilBuff extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'implement',
+		aliases: ['implementationlog', 'implementlog', 'implementation'],
+            group: 'link',
+            memberName: 'implement',
+            description: 'link to wiki implementation log',
+		examples: ['&classbuff'],
+        });
+    }
+
+    async run(message, input) {
+		message.channel.send('https://aigis.fandom.com/wiki/Implementation_Log')
+    }
+}
+module.exports = UtilBuff;
