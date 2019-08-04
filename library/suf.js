464d85868a6f177a53161b6f92f24cb64c71bb68
anyabot <52899201+anyabot@users.noreply.github.com>
2019-08-04 21:46:58 +0900

    Update suf.js

--- library/suf.js
+++ library/suf.js
@@ -1,19 +1,26 @@
 var suffix = {
     '7sins' : '【7Sins】',
     'Christmas' : '(Christmas)',
+    '🎅' : '(Christmas)'
     'Dress' : '(Dress)',
     'Yukata' : '(Yukata)',
     'Swimsuit' : '(Swimsuit)',
+    '👙' : '(Swimsuit)',
+    'Ss' : '(Swimsuit)',
+    '(Ss)' : '(Swimsuit)',
     'Valentine\'s' : '(Valentine\'s)',
     'Valentine' : '(Valentine\'s)',
     '❤' : '(Valentine\'s)',
+    '🏫' : '(School)',
+    '🎒' : '(School)',
     'School' : '(School)',
     'Bride' : '(Bride)',
+    '💍' : '(Bride)',
+    '👰' : '(Bride)',
     'Halloween' : '(Halloween)',
+    '👻' : '(Halloween)',
     'Ny' : '(New Year\'s)',
-    '(Ny)' : '(New Year\'s)',
-    'Ss' : '(Swimsuit)',
-    '(Ss)' : '(Swimsuit)'
+    '(Ny)' : '(New Year\'s)'
 }
 
 exports.suffix = suffix;
