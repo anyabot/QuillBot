7eed4eb638ef5bfd89d5c78c3cb0332643040fc8
anyabot <52899201+anyabot@users.noreply.github.com>
2019-08-06 22:13:43 +0900

    Create iplat.js

---
+++ roll/iplat.js
@@ -0,0 +1,16 @@
+var iplat = {
+    1 : 'Christa',
+    2 : 'Liza',
+    3 : 'Leora',
+    4 : 'Marika',
+    5 : 'Viola',
+	6 : 'Weiss',
+	7 : 'Hilde',
+	8 : 'Tanya',
+	9 : 'Helmine',
+	10 : 'Roza',
+	11 : 'Ralph',
+	12 : 'Motley'
+}
+
+exports.iplat = iplat;
