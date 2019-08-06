5439f89b6eeb932c23a4a7bd4e50792249fc2a9a
anyabot <52899201+anyabot@users.noreply.github.com>
2019-08-06 23:27:18 +0900

    Update barrack.js

--- commands/roll/barrack.js
+++ commands/roll/barrack.js
@@ -36,7 +36,7 @@ class RanRoll extends commando.Command {
 				if (j%15 == 0 && j != 0) {
 					pages.push(embed)
 					embed = new Discord.RichEmbed()
-					embed.setTitle("<@" + message.author.id + ">'s Barrack")
+					embed.setTitle("<@" + message.author.tag + ">'s Barrack")
 					embed.addField("Unit No." + (j + 1), ubarrack[j], true)
 				}
 				else {
