8b06f17d2ecefb499feca5e7a2a8ea456100ce5c
anyabot <52899201+anyabot@users.noreply.github.com>
2019-08-09 23:34:32 +0900

    Create waifu.js

---
+++ commands/roll/waifu.js
@@ -0,0 +1,57 @@
+const commando = require('discord.js-commando');
+const Discord = require('discord.js');
+const Keyv = require('keyv');
+require('@keyv/mysql')
+require('@keyv/mongo')
+var request = require('request');
+var cheerio = require('cheerio');
+var urlencode = require('urlencode');
+
+
+
+class RanRoll extends commando.Command {
+    constructor(client) {
+        super(client, {
+            	name: 'waifu',
+            	group: 'roll',
+            	memberName: 'waifu',
+            	description: 'show your waifu',
+		examples: ['&waifu']
+        });
+    }
+
+    async run(message, input) {
+		const waifu = new Keyv(process.env.MONGODB_URI, { namespace: 'waifu' });
+	    waifu.on('error', err => console.error('Keyv connection error:', err));
+		var uwaifu = await waifu.get(message.author.id)
+		if (uwaifu == undefined) {
+			var mes = "You haven't set your waifu."
+			message.channel.send(mes)
+		}
+		else {
+			var name
+			var unit = uwaifu[0]
+			var state = uwaifu[1]
+			if (state == "BASE") {
+				name = unit
+			}
+			else {
+				name = unit + " " + state
+			}
+			var link = "https://aigis.fandom.com/wiki/File:" + urlencode(name) + "_Render.png";
+				request(link, function(err, resp, html) {
+				if (!err) {
+					const $ = cheerio.load(html);
+					var img = $('.fullImageLink a').attr('href')
+					if (img) {
+						var embed = new Discord.RichEmbed()
+						embed.setTitle(unit)
+						embed.setImage(img)
+						message.channel.send(embed);
+					}
+				}
+			})
+		}
+	}
+}
+module.exports = RanRoll;
