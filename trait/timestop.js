c88bb4ea8b519f5114ccbc899266f66a7947cb8a
anyabot <52899201+anyabot@users.noreply.github.com>
2019-08-12 16:53:58 +0900

    Update timestop.js

--- trait/timestop.js
+++ trait/timestop.js
@@ -1,3 +1,3 @@
-var list = ['Kanon (Christmas)', 'Melvina', 'Shu-Xian', 'Rakshasa', 'Anita', 'Early', 'Faust', 'Giovanni (School)', 'Kokoro', 'Motley', 'Sieglinde', 'Sonken Bundai'];
+var list = ['Kanon (Christmas)', 'Melvina', 'Shu-Xian', 'Rakshasa', 'Anita', 'Early', 'Faust', 'Giovanni (School)', 'Kokoro', 'Mikoto', 'Motley', 'Sieglinde', 'Sonken Bundai'];
 
 exports.list = list;
