dbd1ae9e9bb3be7d6fb0ec0bc06f6c71b364c7b4
anyabot <52899201+anyabot@users.noreply.github.com>
2019-08-12 13:10:18 +0900

    Create trait.js

---
+++ commands/list/trait.js
@@ -0,0 +1,33 @@
+const commando = require('discord.js-commando');
+const Discord = require('discord.js');
+class Trait extends commando.Command {
+    constructor(client) {
+        super(client, {
+            	name: 'trait',
+            	group: 'list',
+            	memberName: 'trait',
+            	description: 'list all classes/units with a certain trait',
+		examples: ['&trait tokenization'],
+		args: [{
+		    key: 'text',
+			prompt: 'What trait do you want to know about?',
+		    type: 'string',
+		default: "all"
+		}]
+        });
+    }
+	async run(message, { text }) {
+		var tr = text.toLowerCase()
+		var path = '/../../trait/' + tr + '.js'
+		fs.stat(path, (err, stats) => {
+			if ( !stats.isFile(filename) ) {
+				var list = require(path).list
+				message.channel.send(list.join("\n"))
+			}  
+			else {
+				message.channel.send("No Data")
+			}
+		})
+	}
+}
+module.exports = Trait;
