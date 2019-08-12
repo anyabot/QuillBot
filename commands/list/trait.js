b6dd504771d596fa43f373d804d60d811f94752d
anyabot <52899201+anyabot@users.noreply.github.com>
2019-08-12 13:28:29 +0900

    Update trait.js

--- commands/list/trait.js
+++ commands/list/trait.js
@@ -18,7 +18,7 @@ class Trait extends commando.Command {
 	async run(message, { text }) {
 		var tr = text.toLowerCase()
 		message.channel.send(tr)
-		var path = '/../../trait/' + tr + '.js'
+		var path = '../../trait/' + tr + '.js'
 		message.channel.send(path)
 		if (moduleIsAvailable(path)) {
 			var list = require(path).list
