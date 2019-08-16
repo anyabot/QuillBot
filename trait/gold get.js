58cef1173806463d63333b91b17fb9ca7d3b5346
anyabot <52899201+anyabot@users.noreply.github.com>
2019-08-16 22:19:08 +0900

    Add files via upload

---
+++ trait/gold get.js
@@ -0,0 +1,3 @@
+var list = ['Bandits 【7Sins】', 'Destroyers (Bandits)', 'Phantom Thieves (GBM)', 'Thieves', 'Harissa',  'Mortimer',  'Thelma',  'Leeanne (New Year\'s)',  'Betty', 'Giovanni (School)', 'Maurette', 'Mammon', 'Patra', 'Tanya', 'Totono (New Year\'s)', 'Totono', 'Lepuru', 'Daniela (Yukata)', 'Merchants\' token', 'Golden Mimics (Saval\'s token)'];
+
+exports.list = list;
