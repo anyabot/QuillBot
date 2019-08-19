27e91460fde009d72efc331818d6e04c0647cdaa
anyabot <52899201+anyabot@users.noreply.github.com>
2019-08-19 18:15:59 +0900

    Create eventblack.js

---
+++ roll/eventblack.js
@@ -0,0 +1,30 @@
+var eventblack = {
+    1 : 'Anya',
+    2 : 'Saki',
+    3 : 'Karma (Black)',
+    4 : 'Anna',
+    5 : 'Liana',
+	6 : 'Nagi',
+	7 : 'Eterna',
+	8 : 'Metus',
+	9 : 'Reshia',
+	10 : 'Hibari',
+	11 : 'Shino',
+	12 : 'White Emperor',
+	13 : 'Anna (Swimsuit)',
+	14 : 'Hakunokami',
+	15 : 'Anatolia',
+	16 : 'Saval',
+	17 : 'Farne (Bride)',
+	18 : 'Noire',
+	19 : 'Shadia',
+	20 : 'Berna (Bride)',
+	21 : 'Anna (Idol)',
+	22 : 'Anna (Uniform)',
+	23 : 'Dark Knight',
+	24 : 'Cynthia',
+	25 : 'Xing-Fu',
+	26 : 'Anna (Waitress)'
+}
+
+exports.eventblack = eventblack;
