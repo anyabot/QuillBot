13c61d74c4fe6c8d55deb0ef366039a3c3706103
anyabot <52899201+anyabot@users.noreply.github.com>
2019-08-27 14:29:58 +0900

    Update find_list.js

--- commands/find/find_list.js
+++ commands/find/find_list.js
@@ -27,6 +27,7 @@ class FindImage extends commando.Command {
 
     async run(message, { text }) {
         var cl = text.toLowerCase().toTitleCase();
+	    cl = cl.split(' And ').join(' and ')
 	    var words = cl.split(' ');
 	    var le = words.length;
 	    if (cl == "Intermediate Dragon Soldier" || cl == "Intermediate Dragon Soldiers") {cl = "Dragon Soldiers"}
@@ -36,7 +37,6 @@ else if (cl == "Kingdom Of Pars" || cl == "Pars" || cl == "Kingdom of Pars") {cl
 else if (cl == "Angels Races") {cl = "Angels Race"}
 else if (cl == "Hermits Races") {cl = "Hermits Race"}
 	    else if (cl == "Spirit of War") {cl = "Spirits of War"}
-	    else if (cl == "Sly And Cunning Genius 【Koihime】" || cl == "Sly And Cunning Genius Koihime" || cl == "Sly And Cunning Geniuses Koihime" || cl == "Sly and Cunning Genius 【Koihime】" || cl == "Sly and Cunning Genius Koihime" || cl == "Sly and Cunning Geniuses Koihime") {cl = "Sly and Cunning Geniuses 【Koihime】"}
 	    else if (cl == "June Bride" || cl == "Summer" || cl == "School" || cl == "Valentine's Day" || cl == "New Year's" || cl == "Christmas" || cl == "Halloween" || cl == "Chibi") {cl = cl + " Units"}
 else if (cl == "Majin" || cl == "Jiangshi" || cl == "Zhenren" || cl == "Onmyouji" || cl == "Youkai" || cl == "Youko" || cl == "Fuuhaku" || cl == "Raikou" || cl == "Nekomata" || cl == "Nurarihyon" || cl == "Oni" || cl == "Tengu" || cl == "Beastfolk" || cl == "None" || cl == "Undead" || cl == "Desert Country" || cl == "Samurai" || cl == "Heavy Artillery") {
 				cl = cl;
