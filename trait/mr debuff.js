7cb7a600921ba84629e15db364b5e213a417e99c
anyabot <52899201+anyabot@users.noreply.github.com>
2019-08-31 13:25:34 +0900

    Update mr debuff.js

--- trait/mr debuff.js
+++ trait/mr debuff.js
@@ -1,3 +1,3 @@
-var list = ['Tenma', 'Ingrid', 'Suiren', 'Adelaide', 'Odette', 'Ryuryu (Valentine\'s)', 'Koyomi', 'Mikoto (Swimsuit)', 'Estelle', 'Sephire', 'Sylseth', 'Wangfei Ren', 'Mithria', 'Olivier (Bride)', 'Honey'];
+var list = ['Tenma', 'Ingrid', 'Suiren', 'Adelaide', 'Odette', 'Ryuryu (Valentine\'s)', 'Koyomi', 'Mikoto (Swimsuit)', 'Estelle', 'Sephire', 'Sylseth', 'Wangfei Ren', 'Mithria', 'Olivier (Bride)', 'Honey', 'Erlang Shen'];
 
 exports.list = list;
