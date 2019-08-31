310ae596cc4bd03528a34af4074ecabf73792b9d
anyabot <52899201+anyabot@users.noreply.github.com>
2019-08-31 11:18:45 +0900

    Update find_mat.js

--- commands/find/find_mat.js
+++ commands/find/find_mat.js
@@ -4,8 +4,7 @@ var request = require('request');
 var cheerio = require('cheerio');
 var he = require('he');
 var pluralize = require('pluralize')
-var name = require('../../library/lib.js').name;
-var suffix = require('../../library/suf.js').suffix;
+var functions = require('../../functions.js');
 require('@gouch/to-title-case')
 var urlencode = require('urlencode');
 
@@ -172,36 +171,7 @@ class FindMat extends commando.Command {
     }
 
     async run(message, { text }) {
-        var unit = text.toLowerCase().toTitleCase();
-	    var np = unit.split(' ');
-	    var npl = np.length;
-	    if (npl >= 2) {
-	    	if (suffix[np[npl-1]]) {
-			np[npl-1] = suffix[np[npl-1]]
-			let sur = np[npl-1]
-			np.pop()
-			let un = np.join(' ')
-			if (name[un]) {un = name[un]}
-			unit = un + ' ' + sur
-		}
-	    	if (np[npl-1] == 'Year' && np[npl-2] == 'New') {
-			let sur = '(New Year\'s)'
-			np.pop()
-			np.pop()
-			let un = np.join(' ')
-			if (name[un]) {un = name[un]}
-			unit = un + ' ' + sur
-		}
-		    if (np[npl-1] == 'Year)' && np[npl-2] == '(New') {
-			let sur = '(New Year\'s)'
-			np.pop()
-			np.pop()
-			let un = np.join(' ')
-			if (name[un]) {un = name[un]}
-			unit = un + ' ' + sur
-		}
-	    }
-        if (name[unit]) unit = name[unit];
+        var unit = functions.nameChange(text)
         var link = "https://aigis.fandom.com/wiki/" + urlencode(unit);
 
 request(link, function(err, resp, html) {
