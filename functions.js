dd47f1ba255e56c774478e67913d28070269dd44
anyabot <52899201+anyabot@users.noreply.github.com>
2019-08-31 11:04:53 +0900

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,7 +1,37 @@
-// capitalize
-exports.toTitleCase = function toTitleCase(str) {
-    return str.replace(/(?![of])(\w+)('s)?('u)?/g, function (txt) {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-    });
+var name = require('library/lib.js').name;
+var suffix = require('library/suf.js').suffix;
+
+exports.nameChange = function nameChange(text) {
+        var unit = text.toLowerCase().toTitleCase();
+	    var np = unit.split(' ');
+	    var npl = np.length;
+	    if (npl >= 2) {
+	    	if (suffix[np[npl-1]]) {
+				np[npl-1] = suffix[np[npl-1]]
+				let sur = np[npl-1]
+				np.pop()
+				let un = np.join(' ')
+				if (name[un]) {un = name[un]}
+				unit = un + ' ' + sur
+			}
+	    	if (np[npl-1] == 'Year' && np[npl-2] == 'New') {
+				let sur = '(New Year\'s)'
+				np.pop()
+				np.pop()
+				let un = np.join(' ')
+				if (name[un]) {un = name[un]}
+				unit = un + ' ' + sur
+			}
+		    if (np[npl-1] == 'Year)' && np[npl-2] == '(New') {
+				let sur = '(New Year\'s)'
+				np.pop()
+				np.pop()
+				let un = np.join(' ')
+				if (name[un]) {un = name[un]}
+				unit = un + ' ' + sur
+			}
+	    }
+        if (name[unit]) unit = name[unit];
+		return unit
 }
 
