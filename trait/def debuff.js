1cbcd34ed747c6c2bea5ecd0033e46d789f11244
anyabot <52899201+anyabot@users.noreply.github.com>
2019-08-31 13:26:03 +0900

    Update def debuff.js

--- trait/def debuff.js
+++ trait/def debuff.js
@@ -1,3 +1,3 @@
-var list = ['Alchemists', 'Nekomata', 'Playwrights (GBM)', 'Puppet Dolls 【Fuukan】', 'Sky Gods (Tengu)', 'Adelaide', 'Helmine', 'Hyuna', 'Koyomi', 'Prince (Dark)', 'Charles', 'Fistia', 'Stream Tank (Machina\'s token)', 'Houtou'];
+var list = ['Alchemists', 'Nekomata', 'Playwrights (GBM)', 'Puppet Dolls 【Fuukan】', 'Sky Gods (Tengu)', 'Adelaide', 'Helmine', 'Hyuna', 'Koyomi', 'Prince (Dark)', 'Charles', 'Fistia', 'Stream Tank (Machina\'s token)', 'Houtou', 'Erlang Shen'];
 
 exports.list = list;
