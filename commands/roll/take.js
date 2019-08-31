c42a06cb25fbb7a9d53edda8fedcaf4e58cc6cea
anyabot <52899201+anyabot@users.noreply.github.com>
2019-08-31 11:32:56 +0900

    Update take.js

--- commands/roll/take.js
+++ commands/roll/take.js
@@ -4,7 +4,7 @@ const random = require('random')
 const Keyv = require('keyv');
 require('@keyv/mysql')
 require('@keyv/mongo')
-
+var functions = require('../../functions.js');
 
 
 class RanRoll extends commando.Command {
@@ -15,15 +15,15 @@ class RanRoll extends commando.Command {
             	memberName: 'take',
             	description: 'take a unit from your previous roll to the barrack (max capacity 100 units)',
               args: [{
-		    key: 'ind',
+		    key: 'text',
 			prompt: 'Which unit do you want to take? (Input the index number from &lastroll)',
-		    type: 'integer'
+		    type: 'string'
 		}],
 		examples: ['&take 1']
         });
     }
 
-    async run(message, { ind }) {
+    async run(message, { text }) {
 		const lastroll = new Keyv(process.env.MONGODB_URI, { namespace: 'lastroll' });
 	    lastroll.on('error', err => console.error('Keyv connection error:', err));
 		const barrack = new Keyv(process.env.MONGODB_URI, { namespace: 'barrack' });
@@ -35,39 +35,77 @@ class RanRoll extends commando.Command {
 	    if (ulastroll == [] || ulastroll == null || ulastroll.length == 0) {
 			message.reply("You have no unit to take")
 		} 
-	    else if (ind < 1 || ind > ulastroll.length) {message.reply("Wrong Index")}
 		else if (ubarrack.length > 99) {message.reply("Your barrack is full")}
-		else {
-			var unit = ulastroll[ind-1]
-			var mes = "Do you want to take " + unit + " to your barrack? (y/n)"
-			message.channel.send(mes).then(msg => {
+	    else if (isNaN(text)) {
+			var ind = Integer.parseInt(text)
+			if (ind < 1 || ind > ulastroll.length) {message.reply("Wrong Index")}
+			else {
+				var unit = ulastroll[ind-1]
+				var mes = "Do you want to take " + unit + " to your barrack? (y/n)"
+				message.channel.send(mes).then(msg => {
 
-				msg.react('🇾').then( r => {
-					msg.react('🇳')
+					msg.react('🇾').then( r => {
+						msg.react('🇳')
 
-					// Filters
-					const backwardsFilter = (reaction, user) => (reaction.emoji.name === '🇾' || reaction.emoji.name === '🇳') && user.id === message.author.id;
+						// Filters
+						const backwardsFilter = (reaction, user) => (reaction.emoji.name === '🇾' || reaction.emoji.name === '🇳') && user.id === message.author.id;
 
-					const backwards = msg.createReactionCollector(backwardsFilter, {timer: 6000 , max: 1});
+						const backwards = msg.createReactionCollector(backwardsFilter, {timer: 6000 , max: 1});
 
-					backwards.on('collect', r => {
-						r.remove(r.users.filter(u => !u.bot).first());
-							if (r.emoji.name === "🇳") {
-							msg.edit("Cancelled")
-						}
-						else if (r.emoji.name === "🇾") {
-							ubarrack.push(unit)
-							msg.edit("You took " + unit + " to your barrack")
-							for (var i = ind - 1; i < ulastroll.length - 1; i++) {
-								ulastroll[i] = ulastroll[i+1]
+						backwards.on('collect', r => {
+							r.remove(r.users.filter(u => !u.bot).first());
+								if (r.emoji.name === "🇳") {
+								msg.edit("Cancelled")
 							}
-							ulastroll.pop()
-							lastroll.set(message.author.id, ulastroll)
-							barrack.set(message.author.id, ubarrack)
-						}
+							else if (r.emoji.name === "🇾") {
+								ubarrack.push(unit)
+								msg.edit("You took " + unit + " to your barrack")
+								for (var i = ind - 1; i < ulastroll.length - 1; i++) {
+									ulastroll[i] = ulastroll[i+1]
+								}
+								ulastroll.pop()
+								lastroll.set(message.author.id, ulastroll)
+								barrack.set(message.author.id, ubarrack)
+							}
+						})
 					})
 				})
-			})
+			}
+		}
+		else {
+			var unit = functions.nameChange(text)
+			if (ulastroll.includes(unit)) {
+				var ind = ulastroll.indexOf(unit) + 1
+				var mes = "Do you want to take " + unit + " to your barrack? (y/n)"
+				message.channel.send(mes).then(msg => {
+
+					msg.react('🇾').then( r => {
+						msg.react('🇳')
+
+						// Filters
+						const backwardsFilter = (reaction, user) => (reaction.emoji.name === '🇾' || reaction.emoji.name === '🇳') && user.id === message.author.id;
+
+						const backwards = msg.createReactionCollector(backwardsFilter, {timer: 6000 , max: 1});
+
+						backwards.on('collect', r => {
+							r.remove(r.users.filter(u => !u.bot).first());
+								if (r.emoji.name === "🇳") {
+								msg.edit("Cancelled")
+							}
+							else if (r.emoji.name === "🇾") {
+								ubarrack.push(unit)
+								msg.edit("You took " + unit + " to your barrack")
+								for (var i = ind - 1; i < ulastroll.length - 1; i++) {
+									ulastroll[i] = ulastroll[i+1]
+								}
+								ulastroll.pop()
+								lastroll.set(message.author.id, ulastroll)
+								barrack.set(message.author.id, ubarrack)
+							}
+						})
+					})
+				})
+			}
 		}
 	}
 }
