c8070eddde7a0dde77bea24bea18b82b3509cfd0
anyabot <52899201+anyabot@users.noreply.github.com>
2019-08-31 11:11:21 +0900

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
-var name = require('/library/lib.js').name;
-var suffix = require('/library/suf.js').suffix;
+var name = require('__dirname/library/lib.js').name;
+var suffix = require('__dirname/library/suf.js').suffix;
 require('@gouch/to-title-case')
 
 exports.nameChange = function nameChange(text) {
