d3d267557ca5ebe77ae486f70099867748e71c80
anyabot <52899201+anyabot@users.noreply.github.com>
2019-08-31 17:17:45 +0900

    Create quiz.js

---
+++ commands/roll/quiz.js
@@ -0,0 +1,66 @@
+const commando = require('discord.js-commando');
+const Discord = require('discord.js');
+var request = require('request');
+var cheerio = require('cheerio');
+const random = require('random')
+var urlencode = require('urlencode');
+const Keyv = require('keyv');
+require('@keyv/mysql')
+require('@keyv/mongo')
+var functions = require('../../functions.js');
+
+
+class RanRoll extends commando.Command {
+    constructor(client) {
+        super(client, {
+            	name: 'quiz',
+            	group: 'roll2',
+            	memberName: 'quiz',
+            	description: 'just quiz',
+		throttling: {
+		usages: 1,
+		duration: 60
+	},
+		examples: ['&quiz']
+        });
+    }
+
+    async run(message, input) {
+	    var link = "https://aigis.fandom.com/wiki/User_blog:Altter/Testing"
+
+		request(link, function(err, resp, html) {
+			if (!err) {
+				const $ = cheerio.load(html);
+				sendembed($, message)
+			}
+		})
+	}
+}
+function sendembed($, message) {
+	var len = $('.image.image-thumbnail.link-internal').length
+	var ind = random.int(1, len)
+	var unit = $('.image.image-thumbnail.link-internal').eq(ind).attr('title')
+	var link2 = "https://aigis.fandom.com/wiki/File:" + urlencode(unit) + "_Render.png";
+	request(link2, function(err, resp, html) {
+		if (!err) {
+			const $2 = cheerio.load(html);
+			var img = $2('.fullImageLink a').attr('href')
+			const filter = response => {
+				let nam = functions.nameChange(response.content)
+				return unit == nam
+			};
+			let embed = new Discord.RichEmbed()
+			embed.setImage(img)
+			message.channel.send(embed).then(() => {
+			message.channel.awaitMessages(filter, { maxMatches: 1, time: 30000, errors: ['time'] })
+				.then(collected => {
+					message.channel.send(collected.first().author + 'got the correct answer!');
+				})
+				.catch(collected => {
+					message.channel.send('Looks like nobody got the answer this time.');
+				});
+			});
+		}
+	})
+}
+module.exports = RanRoll;
