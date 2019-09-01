1e1603c7fb0cc971b01e8ae3faad5d2b55119c59
anyabot <52899201+anyabot@users.noreply.github.com>
2019-09-02 01:43:47 +0900

    Update myteam.js

--- commands/team/myteam.js
+++ commands/team/myteam.js
@@ -58,7 +58,6 @@ async function addimg(uteam, message, i, canvas, ctx) {
 		    var canvas2 = Canvas.createCanvas(img.width, img.height);
 		    var ctx2 = canvas2.getContext('2d');
 		    ctx2.drawImage(img, 0, 0)
-		    ctx2.scale(-1, -1);
 	    ctx.drawImage(canvas2, xy[i][0], xy[i][1])
 		addimg(uteam, message, i + 1, canvas, ctx)
 	    }
