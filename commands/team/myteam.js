dcc68ce91d6cd798bcfab9f25d85b1ef0445a148
anyabot <52899201+anyabot@users.noreply.github.com>
2019-09-01 22:00:04 +0900

    Update myteam.js

--- commands/team/myteam.js
+++ commands/team/myteam.js
@@ -11,6 +11,7 @@ const Canvas = require('canvas');
 var functions = require('../../functions.js');
 var fs = require('fs');
 
+
 class RanRoll extends commando.Command {
     constructor(client) {
         super(client, {
@@ -24,10 +25,20 @@ class RanRoll extends commando.Command {
     }
 
     async run(message, input) {
+	    const xy = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
+	    const team = new Keyv(process.env.MONGODB_URI, { namespace: 'team' });
+	          team.on('error', err => console.error('Keyv connection error:', err));
+            var uteam = await team.get(message.author.id)
+            if (uteam == undefined) {uteam = []}
 	    const canvas = Canvas.createCanvas(583, 426);
 	    const ctx = canvas.getContext('2d');
       const background = await Canvas.loadImage(__dirname + '/../../image/unknown.png');
-      ctx.drawImage(background, 0, 0)
+	    ctx.drawImage(background, 0, 0)
+	    var uimg = []
+	    for (var i = 0; i < uteam.length; i++) {
+	    	let img = Canvas.loadImage(uteam[i])
+		ctx.drawImage(img, xy[i][0], xy[i][1])
+	    }
       const attachment = new Discord.Attachment(canvas.toBuffer(), 'unknown.png');
 	    message.channel.send(attachment);
 	}
