c7e25af8b2ca430cdb39dd13597c19aa6d9bb940
anyabot <52899201+anyabot@users.noreply.github.com>
2019-09-02 20:01:17 +0900

    Update myteam.js

--- commands/team/myteam.js
+++ commands/team/myteam.js
@@ -11,6 +11,7 @@ const Canvas = require('canvas');
 var functions = require('../../functions.js');
 var fs = require('fs');
 const xy = [ [ 466, 306 ], [ 354, 306 ], [ 242, 306 ], [ 130, 306 ], [ 18, 306 ], [ 466, 192 ], [ 354, 192 ], [ 242, 192 ], [ 130, 192 ], [ 18, 192 ], [ 466, 78 ], [ 354, 78 ], [ 242, 78 ], [ 130, 78 ], [ 18, 78 ] ]
+const xy2 = [ [ 108, 108 ], [ 220, 108 ], [ 332, 108 ], [ 444, 108 ], [ 556, 108 ], [ 108, 220 ], [ 220, 220 ], [ 332, 220 ], [ 444, 220 ], [ 556, 220 ], [ 108, 332 ], [ 220, 332 ], [ 332, 332 ], [ 444, 332 ], [ 556, 332 ] ]
 class RanRoll extends commando.Command {
     constructor(client) {
         super(client, {
@@ -53,36 +54,57 @@ class RanRoll extends commando.Command {
 			ctx.drawImage(background, 0, 0)
 			ctx.translate(583, 426);
 			ctx.scale(-1, -1);
-			addimg(uteam[teamname]["link"], message, 0, canvas, ctx)
+			addimg(uteam[teamname], message, 0, canvas, ctx)
 		}
 		else {message.channel.send("You have no team with that name")}
 	}
 }
+
 async function addimg(uteam, message, i, canvas, ctx) {
-	if (!(i < uteam.length)) {
+	if (!(i < uteam["link"].length)) {
+		ctx.scale(-1, -1);
+		addicon(uteam, message, 0, canvas, ctx)
+	}
+	else {
+		var options = {
+			uri: uteam["link"][i],
+			method: "get",
+			encoding: null
+		};
+		request(options, function (error, response, body) {
+			if (error) {
+			} 
+			else {
+			var img = new Canvas.Image();
+			img.src = body;
+			var canvas2 = Canvas.createCanvas(img.width, img.height);
+			var ctx2 = canvas2.getContext('2d');
+			ctx2.translate(img.width, img.height);
+			ctx2.scale(-1, -1);
+			ctx2.drawImage(img, 0, 0)
+			ctx.drawImage(canvas2, xy[i][0], xy[i][1])
+			addimg(uteam, message, i + 1, canvas, ctx)
+			}
+		})
+	}
+}
+async function addicon(uteam, message, i, canvas, ctx) {
+	if (!(i < ["link"].length)) {
 		ctx.scale(-1, -1);
 		const attachment = new Discord.Attachment(canvas.toBuffer(), 'unknown.png');
 	    message.channel.send(attachment);
 	}
-	var options = {
-	    uri: uteam[i],
-	    method: "get",
-	    encoding: null
-	  };
-	  request(options, function (error, response, body) {
-	    if (error) {
-	    } 
-	    else {
-	    var img = new Canvas.Image();
-		img.src = body;
-		    var canvas2 = Canvas.createCanvas(img.width, img.height);
-		    var ctx2 = canvas2.getContext('2d');
-		    ctx2.translate(img.width, img.height);
-		    ctx2.scale(-1, -1);
-		    ctx2.drawImage(img, 0, 0)
-	    ctx.drawImage(canvas2, xy[i][0], xy[i][1])
-		addimg(uteam, message, i + 1, canvas, ctx)
-	    }
-	  })
+	else {
+		var state = uteam["state"][i]
+		if (state == "AW") {
+			var icon = await Canvas.loadImage(__dirname + '/../../image/AW_Icon.png');
+			ctx.drawImage(icon, xy2[i][0], xy2[i][1])
+		}
+		else if (state == "AW2" || state == "AW2v1" || state == "AW2v2") {
+			var icon = await Canvas.loadImage(__dirname + '/../../image/AW2_Icon.png');
+			ctx.drawImage(icon, xy2[i][0], xy2[i][1])
+		}
+		addicon(uteam, message, i + 1, canvas, ctx)
+	}
 }
 module.exports = RanRoll;
