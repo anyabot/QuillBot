f361634dfca35af94bf5bcbca0f84b79e6edf051
anyabot <52899201+anyabot@users.noreply.github.com>
2019-09-03 19:29:42 +0900

    Update dailycredit.js

--- commands/roll/dailycredit.js
+++ commands/roll/dailycredit.js
@@ -32,6 +32,34 @@ class RanRoll extends commando.Command {
 		uday.push(day)
 	    uday.push(month)
 	    uday.push(year)
+	     const quiz = new Keyv(process.env.MONGODB_URI, { namespace: 'quiz' });
+	    quiz.on('error', err => console.error('Keyv connection error:', err));
+	    var uquiz = await quiz.get(message.author.id)
+	    if (uquiz == undefined) {uquiz = []}
+			var score = await quiz.get("score")
+			if (score == undefined) {score = {}}
+			if (!score[message.author.id]) {score[message.author.id] = 0}
+		var items = Object.keys(score).map(function(key) {
+			return [key, score[key]];
+		});
+		items.sort(function(first, second) {
+			return second[1] - first[1];
+		});
+	    	var urank = items.length
+	    	var uscore = score[message.author.id]
+	    	for (var i = items.length - 1; i >= 0; i --) {
+			if (items[i][0] == message.author.id) {
+				urank = i + 1
+			} 
+			else if (items[i][1] == uscore) {urank = i - 1}
+			else if (items[i][1] > uscore) {break}
+		}
+	    var scoresc = Math.floor(uscore/100)
+	    var ranksc
+	    if (urank == 1) {ranksc = 5}
+	    else if (urank < 4) {ranksc = 3}
+	    else if (urank < 6) {ranksc = 2}
+	    else if (urank < 11) {ranksc = 1}
 		const dsc = new Keyv(process.env.MONGODB_URI, { namespace: 'dsc' });
 	    dsc.on('error', err => console.error('Keyv connection error:', err));
       var ldsc = await dsc.get(message.author.id)
@@ -41,13 +69,13 @@ class RanRoll extends commando.Command {
       var uuser = await user.get(message.author.id)
       if (uuser == undefined) {uuser = [150, 10, 33]}
 	    var usc = uuser[0]
-	    var ge = random.int(40, 60)
+	    var ge = 10
 	    if (ldsc == undefined) {
-		usc = usc + ge
+		usc = usc + ge + scoresc + ranksc
 	    	uuser[0] = usc
 		user.set(message.author.id, uuser)
 		    dsc.set(message.author.id, uday)
-      		message.reply("You got " + ge + " SC\nYou curently have " + usc + " SC")
+      		message.reply("You got " + ge + " SC\nYou got " + scoresc + " from your quiz score (1 for every 100 units correctly guessed)\n You got " + ranksc + " from your quiz rank (1 : 5, 2-3 : 3, 4-5 : 2, 6-10 : 1)\nYou curently have " + usc + " SC")
 	    }
 	    else if (ldsc[0] != uday[0] || ldsc[1] != uday[1] || ldsc[2] != uday[2]) {
 		usc = usc + ge
