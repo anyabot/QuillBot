b20549ab7dd6c83b56967579b678b869a2927094
anyabot <52899201+anyabot@users.noreply.github.com>
2019-09-03 19:31:40 +0900

    Update dailycredit.js

--- commands/roll/dailycredit.js
+++ commands/roll/dailycredit.js
@@ -14,7 +14,7 @@ class RanRoll extends commando.Command {
             	group: 'roll2',
               aliases: ['dailysc'],
             	memberName: 'dailycredit',
-            	description: 'get free 40 - 60 sc for the gacha stimulator once per day (reset at 0:00 GMT+9)',
+            	description: 'get free 10 sc for the gacha stimulator once per day (reset at 0:00 GMT+9), extra for quiz score and rank',
 		examples: ['&dailycredit']
         });
     }
