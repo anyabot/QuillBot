c7f9af4239e78d113d3308827a87316682fd818d
anyabot <52899201+anyabot@users.noreply.github.com>
2019-09-05 20:22:15 +0900

    Update sortie.js

--- commands/team/sortie.js
+++ commands/team/sortie.js
@@ -49,7 +49,10 @@ class RanRoll extends commando.Command {
 				}
 				last = parts.pop()
 				var state = "base"
-				if (last == "aw") {
+				if (last == "cc") {
+					state = "CC"
+				}
+				else if (last == "aw") {
 					state = "AW"
 					aw = true
 				} 
@@ -73,17 +76,67 @@ class RanRoll extends commando.Command {
 				if (limited.includes(unit) && mteam["name"].includes(unit)) {message.channel.send("You can only have one " + unit + " per team")}
 				else if (saw == true && aw == false) {message.channel.send("Only Awakened units can have SAW")}
 				else {
-					if (state == "base") {
-						var link = "https://aigis.fandom.com/wiki/File:" + urlencode(unit) + "_Icon.png" 
-					}
-					else {
-						var link = "https://aigis.fandom.com/wiki/File:" + urlencode(unit) + "_" + urlencode(state) + "_Icon.png" 
-					}
+					var link = "https://aigis.fandom.com/wiki/" + urlencode(unit)
 					request(link, function(err, resp, html) {
 						if (!err) {
 							const $ = cheerio.load(html);
-							var img = $('.fullImageLink a').attr('href')
+							var img 
+							var output
+							var unitcheck = $('.categories').text().includes("Player Units")
+							var black = $('.categories').text().includes("Rarity:Black")
+							var baseimg = ($('.listtable.bgwhite tr:nth-child(3) td:nth-child(2)  div a img').attr('data-src'))
+							var baseimg2 = ($('.listtable.bgwhite tr:nth-child(3) td:nth-child(2)  div a img').attr('src'))
+							var awimg = ($('.c3 td:first-child div a img').attr('data-src'))
+							var awimg2 = ($('.c3 td:first-child div a img').attr('src'))
+							var v1img = ($('.c4 td:first-child div a img').attr('data-src'))
+							var v1img2 = ($('.c4 td:first-child div a img').attr('src'))
+							var v2img = ($('.c5 td:first-child div a img').attr('data-src'))
+							var v2img2 = ($('.c5 td:first-child div a img').attr('src'))
+							if (!unitcheck) {message.channel.send("There is no unit with such name")}
+							else if (black && (state = "AW2")) {message.channel.send("Please specify AW2v1 or AW2v2")}
+							else if (state == "CC" && !($('.c2.numbers').first().text())) {message.channel.send("Unit doesn't have CC")}
+							else if (state == "AW" && !($('.c3.numbers').first().text())) {message.channel.send("Unit doesn't have AW")}
+							else if (state == "AW2v1" && !($('.c4.numbers').first().text())) {message.channel.send("Unit doesn't have AW2v1")}
+							else if (state == "AW2v2" && !($('.c5.numbers').first().text())) {message.channel.send("Unit doesn't have AW2v2")}
+							else if (state == "AW2" && !($('.c5.numbers').first().text()) && !($('.c4.numbers').first().text())) {message.channel.send("Unit doesn't have AW2")}
+							else if (state == "AW2") {
+								if (v2img2) {img = v2img2}
+								else if (v2img) {img = v2img}
+								else if (v1img2) {img = v1img2}
+								else if (v1img) {img = v1img}
+								else if (awimg2) {img = awimg2}
+								else if (awimg) {img = awimg}
+								else if (baseimg2) {img = baseimg2}
+								else if (baseimg) {img = baseimg}
+							}
+							else if (state == "AW2v1") {
+								if (v1img2) {img = v1img2}
+								else if (v1img) {img = v1img}
+								else if (awimg2) {img = awimg2}
+								else if (awimg) {img = awimg}
+								else if (baseimg2) {img = baseimg2}
+								else if (baseimg) {img = baseimg}
+							}
+							else if (state == "AW2v2") {
+								if (v2img2) {img = v2img2}
+								else if (v2img) {img = v2img}
+								else if (awimg2) {img = awimg2}
+								else if (awimg) {img = awimg}
+								else if (baseimg2) {img = baseimg2}
+								else if (baseimg) {img = baseimg}
+							}
+							else if (state == "AW2") {
+								if (awimg2) {img = awimg2}
+								else if (awimg) {img = awimg}
+								else if (baseimg2) {img = baseimg2}
+								else if (baseimg) {img = baseimg}
+							}
+							else {
+								if (baseimg2) {img = baseimg2}
+								else if (baseimg) {img = baseimg}
+							}
 							if (img) {
+								img = img.split('/scale-to-width-down/')[0]
 								mteam["link"].push(img)
 								mteam["name"].push(unit)
 								mteam["state"].push(state)
