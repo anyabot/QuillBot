c2c3a8b6313207adca1dd2545dd73df3ffa396a9
anyabot <52899201+anyabot@users.noreply.github.com>
2019-09-08 02:17:41 +0900

    Update youkai.js

--- buff/youkai.js
+++ buff/youkai.js
@@ -1,3 +1,3 @@
-var list = ['Youkai Supreme Commander Nurarihyon (Nurarihyon)', 'Kasumi', 'Kayou', 'Wangfei Ren', 'Shino'];
+var list = ['Youkai Supreme Commander Nurarihyon (Nurarihyon)', 'Kasumi', 'Kayou', 'Wangfei Ren', 'Shino', 'Sagiri'];
 
 exports.list = list;
