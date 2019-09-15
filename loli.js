9dcaee06f59f63c64904a035c4ce23e130325c46
anyabot <52899201+anyabot@users.noreply.github.com>
2019-09-15 12:08:24 +0900

    Create loli.js

---
+++ loli.js
@@ -0,0 +1,16 @@
+var list = [
+['Aisha (Dragon Loli)', 'https://vignette.wikia.nocookie.net/aigis/images/a/a4/Aisha_Icon.png/revision/latest?cb=20180522205440'],
+['Belphe (Sleepy Loli)', 'https://vignette.wikia.nocookie.net/aigis/images/d/db/Belphe_Icon.png/revision/latest?cb=20180712061153'],
+['Belzetta (Crow Loli)', 'https://vignette.wikia.nocookie.net/aigis/images/a/a9/Belzetta_Icon.png/revision/latest?cb=20180522210413'],
+['Cenote (Water Loli)', 'https://vignette.wikia.nocookie.net/aigis/images/d/db/Cenote_Icon.png/revision/latest?cb=20180522211015'],
+['Char (Mechanical Engineering Loli)', 'https://vignette.wikia.nocookie.net/aigis/images/9/99/Char_Icon.png/revision/latest?cb=20190228062232'],
+['Dolce (Candy Loli)', 'https://vignette.wikia.nocookie.net/aigis/images/6/61/Dolce_Icon.png/revision/latest?cb=20180522212448'],
+['Machina (Chemical Engineering Loli)', 'https://vignette.wikia.nocookie.net/aigis/images/c/cd/Machina_Icon.png/revision/latest?cb=20180522220845'],
+['Minette (Cat Loli)', 'https://vignette.wikia.nocookie.net/aigis/images/e/ee/Minette_Icon.png/revision/latest?cb=20190221063114'],
+['Orochihime (Snake Loli)', 'https://vignette.wikia.nocookie.net/aigis/images/c/c6/Orochihime_Icon.png/revision/latest?cb=20180522221941'],
+['Rischelte (Sword Loli)', 'https://vignette.wikia.nocookie.net/aigis/images/3/3b/Rischelte_Icon.png/revision/latest?cb=20180522222423'],
+['Salus (Chain Loli)', 'https://vignette.wikia.nocookie.net/aigis/images/1/13/Salus_Icon.png/revision/latest?cb=20180522222733'],
+['Teeny (Hammer Loli)', 'https://vignette.wikia.nocookie.net/aigis/images/0/02/Teeny_Icon.png/revision/latest?cb=20180522223544'],
+['Yukihime (Ice Loli)', 'https://vignette.wikia.nocookie.net/aigis/images/5/55/Yukihime_Icon.png/revision/latest?cb=20180913061501'],
+]
+exports.list = list;
