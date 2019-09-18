b1a28a7ccfe075337d2632deb1cffaf53ed62faa
anyabot <52899201+anyabot@users.noreply.github.com>
2019-09-19 01:10:53 +0900

    Update setwaifu.js

--- commands/roll/setwaifu.js
+++ commands/roll/setwaifu.js
@@ -15,15 +15,18 @@ class RanRoll extends commando.Command {
             	memberName: 'setwaifu',
             	description: 'set a character in the barrack as your waifu',
               args: [{
-		    key: 'ind',
+		    key: 'text',
 			prompt: 'Which unit do you want to make your waifu? (Input the index number from &barrack)',
-		    type: 'integer'
+		    type: 'string'
 		}],
 		examples: ['&setwaifu 1']
         });
     }
 
-    async run(message, { ind }) {
+    async run(message, { text }) {
+	    var parts = text.split(" ")
+	    if (parts[0] == "&remove") {parts.shift()}
+	    var text2 = parts.join(" ")
 		const barrack = new Keyv(process.env.MONGODB_URI, { namespace: 'barrack' });
 	    barrack.on('error', err => console.error('Keyv connection error:', err));
 		var ubarrack = await barrack.get(message.author.id)
@@ -34,40 +37,82 @@ class RanRoll extends commando.Command {
 	    if (ubarrack == [] || ubarrack == null || ubarrack.length == 0) {
 			message.reply("You have no unit in the barrack")
 		} 
-		else if (ind < 1 || ind > ubarrack.length) {message.reply("Wrong Index")}
-		else if (uwaifu == undefined) {
-			var unit = ubarrack[ind-1]
-			var mes = "You set " + unit + " as your waifu."
-			message.channel.send(mes)
-			var uunit = [unit, "BASE"]
-			waifu.set(message.author.id, uunit)
-		}
-		else {
-			var unit = ubarrack[ind-1]
-			var mes = "You've already set " + uwaifu[0] + " as your waifu.\nDo you want to change to " + unit + "? (y/n)"
-			message.channel.send(mes).then(msg => {
+		else if (!isNaN(text2)) {
+			var ind = parseInt(text2)
+			else if (ind < 1 || ind > ubarrack.length) {message.reply("Wrong Index")}
+			else if (uwaifu == undefined) {
+				var unit = ubarrack[ind-1]
+				var mes = "You set " + unit + " as your waifu."
+				message.channel.send(mes)
+				var uunit = [unit, "BASE"]
+				waifu.set(message.author.id, uunit)
+			}
+			else {
+				var unit = ubarrack[ind-1]
+				var mes = "You've already set " + uwaifu[0] + " as your waifu.\nDo you want to change to " + unit + "? (y/n)"
+				message.channel.send(mes).then(msg => {
 
-				msg.react('🇾').then( r => {
-					msg.react('🇳')
+					msg.react('🇾').then( r => {
+						msg.react('🇳')
 
-					// Filters
-					const backwardsFilter = (reaction, user) => (reaction.emoji.name === '🇾' || reaction.emoji.name === '🇳') && user.id === message.author.id;
+						// Filters
+						const backwardsFilter = (reaction, user) => (reaction.emoji.name === '🇾' || reaction.emoji.name === '🇳') && user.id === message.author.id;
 
-					const backwards = msg.createReactionCollector(backwardsFilter, {timer: 6000 , max: 1});
+						const backwards = msg.createReactionCollector(backwardsFilter, {timer: 6000 , max: 1});
 
-					backwards.on('collect', r => {
-						r.remove(r.users.filter(u => !u.bot).first());
-							if (r.emoji.name === "🇳") {
-							msg.edit("Cancelled")
-						}
-						else if (r.emoji.name === "🇾") {
-							msg.edit("You set " + unit + " as your waifu.")
-							var uunit = [unit, "BASE"]
-							waifu.set(message.author.id, uunit)
-						}
+						backwards.on('collect', r => {
+							r.remove(r.users.filter(u => !u.bot).first());
+								if (r.emoji.name === "🇳") {
+								msg.edit("Cancelled")
+							}
+							else if (r.emoji.name === "🇾") {
+								msg.edit("You set " + unit + " as your waifu.")
+								var uunit = [unit, "BASE"]
+								waifu.set(message.author.id, uunit)
+							}
+						})
 					})
 				})
-			})
+			}
+		}
+		else {
+			var unit = functions.nameChange(text2)
+			if (ubarrack.includes(unit)) {
+				var ind = ubarrack.indexOf(unit) + 1;
+				if (uwaifu == undefined) {
+					var unit = ubarrack[ind-1]
+					var mes = "You set " + unit + " as your waifu."
+					message.channel.send(mes)
+					var uunit = [unit, "BASE"]
+					waifu.set(message.author.id, uunit)
+				}
+				else {
+					var mes = "You've already set " + uwaifu[0] + " as your waifu.\nDo you want to change to " + unit + "? (y/n)"
+					message.channel.send(mes).then(msg => {
+
+						msg.react('🇾').then( r => {
+							msg.react('🇳')
+
+							// Filters
+							const backwardsFilter = (reaction, user) => (reaction.emoji.name === '🇾' || reaction.emoji.name === '🇳') && user.id === message.author.id;
+
+							const backwards = msg.createReactionCollector(backwardsFilter, {timer: 6000 , max: 1});
+
+							backwards.on('collect', r => {
+								r.remove(r.users.filter(u => !u.bot).first());
+									if (r.emoji.name === "🇳") {
+									msg.edit("Cancelled")
+								}
+								else if (r.emoji.name === "🇾") {
+									msg.edit("You set " + unit + " as your waifu.")
+									var uunit = [unit, "BASE"]
+									waifu.set(message.author.id, uunit)
+								}
+							})
+						})
+					})
+				}
+			}
 		}
 	}
 }
