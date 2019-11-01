fa2d9e8f3c5543bc1e7f80ffaa1678732ae5b7d1
anyabot <52899201+anyabot@users.noreply.github.com>
2019-11-01 11:14:47 +0900

    Update archer.js

--- buff/archer.js
+++ buff/archer.js
@@ -1,3 +1,3 @@
-var list = ['Spica', 'Ashera', 'Prince (Einherjar)', 'Sue', 'Calope'];
+var list = ['Yuugen (Black)', 'Yuugen (Platinum)', 'Spica', 'Ashera', 'Prince (Einherjar)', 'Sue', 'Calope'];
 
 exports.list = list;
