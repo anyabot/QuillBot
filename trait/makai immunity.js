782209e083a5a694bda928cb53ae0231802e209b
anyabot <52899201+anyabot@users.noreply.github.com>
2019-11-01 11:12:20 +0900

    Update makai immunity.js

--- trait/makai immunity.js
+++ trait/makai immunity.js
@@ -1,3 +1,3 @@
-var list = ['Beast Tamers','Chibi Beast Tamers', 'Chibi Demon Masters', 'Dark Priests', 'Dark Stalkers', 'Demon Summoners', 'Dragon Princesses', 'Dragon Shamans', 'Dragon Soldiers', 'Evil Princesses', 'Knights 【7Sins】', 'Majin', 'Prince (Dark)', 'Riele', 'Sharqi', 'Melvina', 'Lythtis', 'Nakia', 'Doula', 'Barbara', 'Linaria', 'Noire', 'Anatolia', 'Arietta', 'Zilva', 'Shadia', 'Melabyss', 'Bestla', 'Ulidem', 'Angeline', 'Prisna', 'Nevin', 'Ilios'];
+var list = ['Beast Tamers','Chibi Beast Tamers', 'Chibi Demon Masters', 'Dark Priests', 'Dark Stalkers', 'Demon Summoners', 'Dragon Princesses', 'Dragon Shamans', 'Dragon Soldiers', 'Evil Princesses', 'Knights 【7Sins】', 'Majin', 'Prince (Dark)', 'Atnates (Black)', 'Atnates (Platinum)','Riele', 'Sharqi', 'Melvina', 'Lythtis', 'Nakia', 'Doula', 'Barbara', 'Linaria', 'Noire', 'Anatolia', 'Arietta', 'Zilva', 'Shadia', 'Melabyss', 'Bestla', 'Ulidem', 'Angeline', 'Prisna', 'Nevin', 'Ilios'];
 
 exports.list = list;
