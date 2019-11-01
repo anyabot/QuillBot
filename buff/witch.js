a619470ff3d1e5d54b4a5cfecfb6a7c2ed0bc9ca
anyabot <52899201+anyabot@users.noreply.github.com>
2019-11-01 11:16:25 +0900

    Update witch.js

--- buff/witch.js
+++ buff/witch.js
@@ -1,3 +1,3 @@
-var list = ['Candy', 'Belinda', 'Chibi Towa', 'Despia', 'Kokoro', 'Towa'];
+var list = ['Solais (Black)', 'Solais (Platinum)', 'Candy', 'Belinda', 'Chibi Towa', 'Despia', 'Kokoro', 'Towa'];
 
 exports.list = list;
