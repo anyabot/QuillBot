0c0d25ec4016242b1bf1084d5e3bce1bed8a7386
anyabot <52899201+anyabot@users.noreply.github.com>
2019-11-01 11:16:01 +0900

    Update mage.js

--- buff/mage.js
+++ buff/mage.js
@@ -1,3 +1,3 @@
-var list = ['Estelle', 'Soleil', 'Garania', 'Ingrid', 'Despia', 'Alma', 'Shirley', 'Mehlis (School)', 'Towa'];
+var list = ['Solais (Black)', 'Solais (Platinum)', 'Estelle', 'Soleil', 'Garania', 'Ingrid', 'Despia', 'Alma', 'Shirley', 'Mehlis (School)', 'Towa'];
 
 exports.list = list;
