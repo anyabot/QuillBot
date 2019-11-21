7aaebbefea22fb675050e873b4cf7772777c8680
anyabot <52899201+anyabot@users.noreply.github.com>
2019-11-21 17:10:53 +0900

    Update dragon.js

--- buff/dragon.js
+++ buff/dragon.js
@@ -1,3 +1,3 @@
-var list = ['Elder Dragon Shamans (Dragon Shamans)', 'Echidna', 'Anya', 'Rubinas', 'Eldora', 'Ignis', 'Meliusiana'];
+var list = ['Elder Dragon Shamans (Dragon Shamans)', 'Echidna', 'Anya', 'Rubinas', 'Eldora', 'Ignis', 'Meliusiana', 'Krasil'];
 
 exports.list = list;
