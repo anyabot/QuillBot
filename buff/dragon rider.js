2d5d30de5c3219def451426752c692a7003b4bc3
anyabot <52899201+anyabot@users.noreply.github.com>
2019-11-21 17:11:02 +0900

    Update dragon rider.js

--- buff/dragon rider.js
+++ buff/dragon rider.js
@@ -1,3 +1,3 @@
-var list = ['Lumian', 'Echidna', 'Anya', 'Alicia', 'Reshia', 'Rion', 'Rubinas', 'Eldora', 'Ignis', 'Meliusiana'];
+var list = ['Lumian', 'Echidna', 'Anya', 'Alicia', 'Reshia', 'Rion', 'Rubinas', 'Eldora', 'Ignis', 'Meliusiana', 'Krasil'];
 
 exports.list = list;
