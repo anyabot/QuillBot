e014bd752bf08b3d8561bd5cc3a226592fb09433
anyabot <52899201+anyabot@users.noreply.github.com>
2019-11-21 17:09:08 +0900

    Update white empire.js

--- buff/white empire.js
+++ buff/white empire.js
@@ -1,3 +1,3 @@
-var list = ['Elias', 'Angeline', 'Marika', 'White Emperor', 'Leona', 'Natalie', 'Kyuubi', 'Diana', 'Eleonora', 'Hilde'];
+var list = ['Elias', 'Angeline', 'Marika', 'White Emperor', 'Leona', 'Natalie', 'Kyuubi', 'Diana', 'Eleonora', 'Hilde', 'Aleida', 'Patricia'];
 
 exports.list = list;
