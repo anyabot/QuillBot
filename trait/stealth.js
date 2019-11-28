78a87c1106de87c0979d2435b9b154d533f19c29
anyabot <52899201+anyabot@users.noreply.github.com>
2019-11-28 21:40:10 +0900

    Update stealth.js

--- trait/stealth.js
+++ trait/stealth.js
@@ -1,3 +1,3 @@
-var list = ['Tengu', '8-Gates Feng Shui Instructors (Feng Shui Users)', 'Nurarihyon', 'Shadow Hunters (Vampire Hunters)', 'Amelie', 'Kurama', 'Fiore', 'Ariel', 'Hatsune', 'Juubei', 'Orihime', 'Prim', 'Prisna', 'Ricola', 'Rita', 'Rita (Swimsuit)', 'Rovinia', 'Sagiri', 'Setsuna', 'Supura', 'Xenia'];
+var list = ['Tengu', '8-Gates Feng Shui Instructors (Feng Shui Users)', 'Nurarihyon', 'Shadow Hunters (Vampire Hunters)', 'Visionaries (Shamans)', 'Amelie', 'Kurama', 'Fiore', 'Ariel', 'Hatsune', 'Juubei', 'Orihime', 'Prim', 'Prisna', 'Ricola', 'Rita', 'Rita (Swimsuit)', 'Rovinia', 'Sagiri', 'Setsuna', 'Supura', 'Xenia'];
 
 exports.list = list;
