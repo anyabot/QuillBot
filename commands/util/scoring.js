a4c5cb56bc37bc6a789aa37019de9a5034d53dd3
anyabot <52899201+anyabot@users.noreply.github.com>
2019-12-08 18:07:47 +0900

    Update scoring.js

--- commands/util/scoring.js
+++ commands/util/scoring.js
@@ -19,6 +19,10 @@ class FindOrb extends commando.Command {
 		  var embed = new Discord.RichEmbed()
 			embed.setTitle('Tower of Heroes Up Scoring')
 			embed.setURL('https://aigis.fandom.com/wiki/The_Tower_of_Heroes')
+			embed.setDescription("Base Amount x Unit Count Modifier x Cost Modifier = Score")
+			embed.addField('Base Amount', '★ : 2500\n★★ : 5000\n★★★ : 10000')
+			embed.addField('Unit Count Modifier', 'If 0 units are present in Team (Prince Only), base multiplier has a value of x2.5\nFor every unit added, -0.1 to base multiplier')
+			embed.addField('Cost Modifier', 'If no Unit Points are expended, base multiplier has a value of x7.5\nFor every 1 UP expended, -0.02 to base multiplier')
 			embed.setImage('https://vignette.wikia.nocookie.net/aigis/images/d/d4/Herotower_scorechart.png/revision/latest?cb=20191031182629')
       message.channel.send(embed)
 	}
