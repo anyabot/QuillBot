fce4c154c299dc86fefae7e2b58f558715b5bedf
anyabot <52899201+anyabot@users.noreply.github.com>
2019-12-12 16:12:51 +0900

    Update tokenization.js

--- trait/tokenization.js
+++ trait/tokenization.js
@@ -1,3 +1,3 @@
-var list = ['Maids', 'Altair', 'Chibi Alttair', 'Mireille', 'Chibi Mireille', 'Reshia', 'Berna', 'Hibari', 'Alice', 'Siena', 'Sereina', 'Eldora', 'Aleese', 'Ema', 'Dorothea', 'Orihime', 'Xenia', 'Shino', 'Kyuubi', 'Alicia (Bride)', 'Towa (School)', 'Eden (Halloween)'];
+var list = ['Maids', 'Altair', 'Chibi Alttair', 'Mireille', 'Chibi Mireille', 'Reshia', 'Berna', 'Hibari', 'Alice', 'Siena', 'Sereina', 'Eldora', 'Aleese', 'Ema', 'Dorothea', 'Orihime', 'Xenia', 'Shino', 'Kyuubi', 'Alicia (Bride)', 'Towa (School)', 'Eden (Halloween)', 'Solano (Christmas)'];
 
 exports.list = list;
