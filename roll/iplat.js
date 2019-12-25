d61d7ab046a5a79ee8ffd3a1ad5e61ab61caad81
anyabot <52899201+anyabot@users.noreply.github.com>
2019-12-25 11:01:53 +0900

    Update iplat.js

--- roll/iplat.js
+++ roll/iplat.js
@@ -1,17 +1,17 @@
-var iplat = {
-    1 : 'Christa',
-    2 : 'Liza',
-    3 : 'Leora',
-    4 : 'Marika',
-    5 : 'Viola',
-	6 : 'Weiss',
-	7 : 'Hilde',
-	8 : 'Tanya',
-	9 : 'Helmine',
-	10 : 'Roza',
-	11 : 'Ralph',
-	12 : 'Motley',
-	13 : 'Patricia'
-}
+var iplat = [
+    	'Christa',
+    	'Liza',
+    	'Leora',
+    	'Marika',
+    	'Viola',
+	'Weiss',
+	'Hilde',
+	'Tanya',
+	'Helmine',
+	'Roza',
+	'Ralph',
+	'Motley',
+	'Patricia'
+]
 
 exports.iplat = iplat;
