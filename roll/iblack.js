2736c85e093fe67d8cf196455c7c3253aabbf9f0
anyabot <52899201+anyabot@users.noreply.github.com>
2019-12-25 11:00:07 +0900

    Update iblack.js

--- roll/iblack.js
+++ roll/iblack.js
@@ -1,14 +1,14 @@
 var iblack = {
-    1 : 'Angeline',
-    2 : 'Diana',
-    3 : 'Elmira',
-    4 : 'Kyuubi',
-    5 : 'Sieglinde',
-	6 : 'Wurm',
-	7 : 'Mephisto',
-	8 : 'Aurora',
-	9 : 'Eleonora',
-	10 : 'Aleida'
+    	'Angeline',
+    	'Diana',
+    	'Elmira',
+    	'Kyuubi',
+    	'Sieglinde',
+	'Wurm',
+	'Mephisto',
+	'Aurora',
+	'Eleonora',
+	'Aleida'
 }
 
 exports.iblack = iblack;
