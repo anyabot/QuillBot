045f1389e15b0525eede6ad5edae536714b15963
anyabot <52899201+anyabot@users.noreply.github.com>
2019-12-25 11:22:27 +0900

    Update isil.js

--- roll/isil.js
+++ roll/isil.js
@@ -1,11 +1,11 @@
-var isil = {
-    1 : 'Clara',
-    2 : 'Dwight',
-    3 : 'Lux',
-    4 : 'Rute',
-    5 : 'Eric',
-	6 : 'Heath',
-	7 : 'Solare'
-}
+var isil = [
+    	'Clara',
+    	'Dwight',
+    	'Lux',
+    	'Rute',
+    	'Eric',
+	'Heath',
+	'Solare'
+]
 
 exports.isil = isil;
