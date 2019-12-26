949a7294c84d0aa8ff448b342a3749ae3d153d05
anyabot <52899201+anyabot@users.noreply.github.com>
2019-12-26 21:51:00 +0900

    Update undead.js

--- buff/undead.js
+++ buff/undead.js
@@ -1,3 +1,3 @@
-var list = ['Carmilla (Vampire Lords)', 'Goethe', 'Lei-Mei', 'Prince (Dark)', 'Metus', 'Ema', 'Melabyss', 'Silvia (Swimsuit)'];
+var list = ['Carmilla (Vampire Lords)', 'Goethe', 'Lei-Mei', 'Prince (Dark)', 'Metus', 'Ema', 'Melabyss', 'Silvia (Swimsuit)', 'Harmonia'];
 
 exports.list = list;
