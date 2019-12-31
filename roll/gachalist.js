0d0e040e1ab07d73ae8748cc0534706e10854e6b
anyabot <52899201+anyabot@users.noreply.github.com>
2020-01-01 00:06:01 +0900

    Update gachalist.js

--- roll/gachalist.js
+++ roll/gachalist.js
@@ -1,9 +1,9 @@
 var gachalist = {
   "pugopen" : true,
   "b1open" : true,
-  "b2open" : true,
+  "b2open" : false,
   "s1open" : false,
-  "s2open" : false,
+  "s2open" : true,
   "i1open" : false,
   "i2open" : false
 }
