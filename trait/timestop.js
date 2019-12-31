0a1b49b037e5f2438f758f5601b64b5ffd1b38ea
anyabot <52899201+anyabot@users.noreply.github.com>
2020-01-01 00:25:08 +0900

    Update timestop.js

--- trait/timestop.js
+++ trait/timestop.js
@@ -1,3 +1,3 @@
-var list = ['Kanon (Christmas)', 'Melvina', 'Shu-Xian', 'Rakshasa', 'Pirika', 'Anita', 'Arietta', 'Chibi Mikoto', 'Early', 'Faust', 'Giovanni (School)', 'Kokoro', 'Lizette', 'Mikoto', 'Motley', 'Sieglinde', 'Sonken Bundai', 'Patricia', 'Evil Eye (Umbre\'s token)', 'Corpse Marionette (Shu-Xian\'s token)'];
+var list = ['Kanon (Christmas)', 'Melvina', 'Shu-Xian', 'Rakshasa', 'Pirika', 'Anita', 'Arietta', 'Chibi Mikoto', 'Early', 'Faust', 'Giovanni (School)', 'Junon (New Year\'s)', 'Kokoro', 'Lizette', 'Mikoto', 'Motley', 'Sieglinde', 'Sonken Bundai', 'Patricia', 'Evil Eye (Umbre\'s token)', 'Corpse Marionette (Shu-Xian\'s token)'];
 
 exports.list = list;
