b48d0080c364212259a93bd9d5888325f413e360
anyabot <52899201+anyabot@users.noreply.github.com>
2020-01-02 02:28:43 +0900

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,7 +1,8 @@
 var name = require('./library/lib.js').name;
 var suffix = require('./library/suf.js').suffix;
 require('@gouch/to-title-case')
-
+const commando = require('discord.js-commando');
+const Discord = require('discord.js');
 exports.nameChange = function nameChange(text) {
         var unit = text.toLowerCase().toTitleCase();
 	    var np = unit.split(' ');
@@ -70,3 +71,43 @@ exports.nameChange = function nameChange(text) {
 		return unit
 }
 
+exports.sende = function sende(message, pages) {
+	var embed = pages[0];
+	let page = 1;
+	embed = pages[0];
+	embed.setFooter('Page ' + page + ' of ' + pages.length);
+	message.channel.send(embed).then(msg => {
+		msg.react('⬅️').then( r => {
+			msg.react('➡️')
+
+			// Filters
+			const backwardsFilter = (reaction, user) => reaction.emoji.name === '⬅️' && !user.bot;
+			const forwardsFilter = (reaction, user) => reaction.emoji.name === '➡️' && !user.bot;
+
+			const backwards = msg.createReactionCollector(backwardsFilter, {timer: 6000});
+			const forwards = msg.createReactionCollector(forwardsFilter, {timer: 6000});
+
+			backwards.on('collect', r => {
+			r.remove(r.users.filter(u => !u.bot).first());
+				if (page === 1) {
+					page = pages.length + 1;
+				}
+				page--;
+					embed = pages[page-1];
+					embed.setFooter('Page ' + page + ' of ' + pages.length);
+					msg.edit(embed)
+			})
+
+			forwards.on('collect', r => {
+			r.remove(r.users.filter(u => !u.bot).first());
+					if (page === pages.length) {
+						page = 0;
+					}
+					page++;
+					embed = pages[page-1];
+					embed.setFooter('Page ' + page + ' of ' + pages.length);
+					msg.edit(embed)
+			})
+		})
+	})
+}
