500f9f09493487688f1effe242912d93103e4081
anyabot <52899201+anyabot@users.noreply.github.com>
2020-01-22 23:28:45 +0900

    Update weather immunity.js

--- trait/weather immunity.js
+++ trait/weather immunity.js
@@ -1,3 +1,3 @@
-var list = ['Rangers', 'High Cannon Masters (Gunners)', 'Longji Gongzhu', 'Spiria', 'Dolce (Swimsuit)', 'Jacqueline', 'Aoba', 'Sanara (Platinum)', 'Kururu (Blizzard only)', 'Sarasa (Blizzard only)', 'Eliza (Blizzard only)', 'Camilla (Christmas) (Blizzard only)', 'Ertel (Christmas) (Blizzard only)'];
+var list = ['Rangers', 'High Cannon Masters (Gunners)', 'Longji Gongzhu', 'Spiria', 'Dolce (Swimsuit)', 'Jacqueline', 'Aoba', 'Sanara (Platinum)', 'Claris Zeinburg', 'Kururu (Blizzard only)', 'Sarasa (Blizzard only)', 'Eliza (Blizzard only)', 'Camilla (Christmas) (Blizzard only)', 'Ertel (Christmas) (Blizzard only)'];
 
 exports.list = list;
