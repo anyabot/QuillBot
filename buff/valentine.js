5e4f89dae703d3c8051dc326afac793545d62c72
anyabot <52899201+anyabot@users.noreply.github.com>
2020-02-20 22:32:03 +0900

    Update valentine.js

--- buff/valentine.js
+++ buff/valentine.js
@@ -1,3 +1,3 @@
-var list = ['Aix (Valentine\'s)', 'Emelda (Valentine\'s)', 'Marnie (Valentine\'s)'];
+var list = ['Aix (Valentine\'s)', 'Emelda (Valentine\'s)', 'Marnie (Valentine\'s)', 'Sharqi (Valentine\'s)'];
 
 exports.list = list;
