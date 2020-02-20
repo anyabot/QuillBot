317042592ec4b4906fb730557fd80649795ceab8
anyabot <52899201+anyabot@users.noreply.github.com>
2020-02-20 22:28:30 +0900

    Update self tokenization.js

--- trait/self tokenization.js
+++ trait/self tokenization.js
@@ -1,3 +1,3 @@
-var list = ['Avengers', 'Chibi Avengers', 'Dark Stalkers', 'Majin', 'Nosferatu (Vampire Princesses)', 'Sayo', 'Barbara', 'Shu-Xian', 'Melvina', 'Prisna', 'Sill', 'Meliodas', 'Alicia (Bride)'];
+var list = ['Avengers', 'Chibi Avengers', 'Dark Stalkers', 'Majin', 'Nosferatu (Vampire Princesses)', 'Demon Rune', 'Sayo', 'Barbara', 'Shu-Xian', 'Melvina', 'Prisna', 'Sill', 'Meliodas', 'Alicia (Bride)'];
 
 exports.list = list;
