a9de66fc5054b2365e4ee3db3103b5c8205d7fa1
anyabot <52899201+anyabot@users.noreply.github.com>
2020-03-05 15:29:28 +0900

    Update taunt.js

--- trait/taunt.js
+++ trait/taunt.js
@@ -1,3 +1,5 @@
-var list = ['Anatolia', 'Benibotan', 'Chibi Chizuru', 'Chizuru', 'Juubei', 'Ilios', 'Lapis (Swimsuit)', 'Leone', 'Miyabi', 'Nakia', 'Palfy', 'Picky', 'Plume', 'Rindou', 'Tigre', 'Totono'];
+var list = ['Anatolia', 'Benibotan', 'Chibi Chizuru', 'Chizuru', 'Juubei', 'Ilios', 'Lapis (Swimsuit)', 'Leone', 'Miyabi', 'Nakia', 'Palfy', 'Picky', 'Plume', 'Rindou', 'Tigre', 'Totono', 
+           'Oviely'
+           ];
 
 exports.list = list;
