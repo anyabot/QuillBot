958a42e69473b88408f1932cd2db329d0a352468
anyabot <52899201+anyabot@users.noreply.github.com>
2020-03-22 22:03:46 +0900

    Update ranged.js

--- buff/ranged.js
+++ buff/ranged.js
@@ -1,3 +1,13 @@
-var list = ['Adelaide', 'Monica', 'Shaina', 'Towa (School)', 'Socie', 'Fiore', 'Ibuki', 'Chibi Ibuki', 'Shinato'];
+var list = ['Adelaide', 
+            'Monica', 
+            'Shaina', 
+            'Towa (School)', 
+            'Socie', 
+            'Fiore', 
+            'Ibuki', 
+            'Chibi Ibuki', 
+            'Shinato',
+            'Eris'
+           ];
 
 exports.list = list;
