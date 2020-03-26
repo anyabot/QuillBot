a8803c750f845fb0db45472b3d6f9b4324dcd318
anyabot <52899201+anyabot@users.noreply.github.com>
2020-03-26 16:20:45 +0900

    Update resurrection.js

--- trait/resurrection.js
+++ trait/resurrection.js
@@ -1,3 +1,13 @@
-var list = ['Chibi Dorothy', 'Dorothy', 'Gustav', 'Hien', 'Longji Gongzhu', 'Nakia', 'Sayo', 'Prince (Einherjar)', 'Sophie (Valentine\'s)'];
+var list = ['Chibi Dorothy', 
+            'Dorothy', 
+            'Gustav', 
+            'Hien', 
+            'Longji Gongzhu', 
+            'Nakia', 
+            'Sayo', 
+            'Prince (Einherjar)', 
+            'Sophie (Valentine\'s)',
+            'Tsugumi'
+           ];
 
 exports.list = list;
