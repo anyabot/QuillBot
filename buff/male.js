38f72460a38bc8f3506be0ff063d9042b82908df
anyabot <52899201+anyabot@users.noreply.github.com>
2020-04-17 11:36:18 +0900

    Update male.js

--- buff/male.js
+++ buff/male.js
@@ -1,3 +1,13 @@
-var list = ['Aix (Valentine\'s)', 'Atnates (Black)', 'Atnates (Platinum)', 'Gordeaux', 'Robert', 'Khuri (Swimsuit)', 'Leaf (Valentine\'s)', 'Oscar', 'Sharqi (Valentine\'s)'];
+var list = ['Aix (Valentine\'s)', 
+            'Atnates (Black)', 
+            'Atnates (Platinum)', 
+            'Gordeaux', 
+            'Robert', 
+            'Khuri (Swimsuit)', 
+            'Leaf (Valentine\'s)', 
+            'Oscar', 
+            'Sharqi (Valentine\'s)',
+            'Mortimer (Female)'
+           ];
 
 exports.list = list;
