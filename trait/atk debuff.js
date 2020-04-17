5aece5794b04216f4c9748eb7a519c3eb8b35541
anyabot <52899201+anyabot@users.noreply.github.com>
2020-04-17 11:40:39 +0900

    Update atk debuff.js

--- trait/atk debuff.js
+++ trait/atk debuff.js
@@ -1,3 +1,26 @@
-var list = ['Curse Users', 'Maou\'s Bodyguards', 'Nekomata', 'Playwrights (GBM)', 'Puppet Dolls 【Fuukan】', 'Orochihime', 'Prince (Dark)', 'Sybilla', 'Yurina', 'Chibi Cornelia', 'Cornelia (Christmas)', 'Cornelia', 'Dark Knight', 'Ban', 'Tamami', 'Aida', 'Hilde', 'Hu Ximei', 'Puniru', 'Erlang Shen', 'Shinomiya Komachi', 'Isambard'];
+var list = ['Curse Users', 
+           'Maou\'s Bodyguards', 
+           'Nekomata', 
+           'Playwrights (GBM)', 
+           'Puppet Dolls 【Fuukan】', 
+           'Orochihime', 
+           'Prince (Dark)', 
+           'Sybilla', 
+           'Yurina', 
+           'Chibi Cornelia', 
+           'Cornelia (Christmas)', 
+           'Cornelia', 
+           'Dark Knight', 
+           'Ban', 
+           'Tamami', 
+           'Aida', 
+           'Hilde', 
+           'Hu Ximei', 
+           'Puniru', 
+           'Erlang Shen', 
+           'Shinomiya Komachi', 
+           'Isambard',
+           'Aira'
+           ];
 
 exports.list = list;
