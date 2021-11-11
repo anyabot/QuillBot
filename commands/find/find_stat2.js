96f87f52f6c3cc1c75c7b5d2301449b71e7889ef
anyabot <52899201+anyabot@users.noreply.github.com>
2021-11-11 23:52:25 +0900

    test

--- commands/find/find_stat2.js
+++ commands/find/find_stat2.js
@@ -29,75 +29,81 @@ class FindStat2 extends commando.Command {
 
   async run(message, { text }) {
     var unit = text;
-    var link = "https://mist-train-girls.fandom.com/wiki/" + urlencode(unit);
+    var pages = [];
     request(link, function (err, resp, html) {
       if (!err) {
         const $ = cheerio.load(html);
-        var pu = $(".categories").text().includes("Layers");
-        if (pu) {
-          var output;
-          var img;
-          var aff;
-          var check = false;
-          var pages = [];
-          output = $(".wikitable.hidden.stats tr:nth-child(4)").html();
-          if (output) {
-            check = true;
-            let line1 = lv1line(
-              $(".wikitable.hidden.stats tr:nth-child(3)").html()
-            );
-            let line2 = lv1line(
-              $(".wikitable.hidden.stats tr:nth-child(4)").html()
-            );
-            let embed = new Discord.RichEmbed()
-              .setURL(link)
-              .setTitle(unit + "'s Stats")
-              .setThumbnail(img)
-              .setColor("LIGHT_GREY")
-              .addField("ATK", line1[1] + " → " + line2[1], true)
-              .addField("DEF", line1[2] + " → " + line2[2], true)
-              .addField("HIT", line1[3] + " → " + line2[3], true)
-              .addField("SPD", line1[4] + " → " + line2[4], true)
-              .addField("MATK", line1[5] + " → " + line2[5], true)
-              .addField("MDEF", line1[6] + " → " + line2[6], true)
-              .addField("RCV", line1[7] + " → " + line2[7], true)
-              .addField("LUK", line1[8] + " → " + line2[8], true)
-            pages.push(embed);
-          }
-					output = $(".wikitable.hidden.resist tr:nth-child(2)").html();
-          if (output) {
-            check = true;
-            let line1 = lv1line(
-              $(".wikitable.hidden.resist tr:nth-child(2)").html()
-            );
-            let embed = new Discord.RichEmbed()
-              .setURL(link)
-              .setTitle(unit + "'s Resists")
-              .setThumbnail(img)
-              .setColor("LIGHT_GREY")
-							.addField("Cut", line1[0], true)
-              .addField("Blow", line1[1], true)
-              .addField("Pierce", line1[2], true)
-              .addField("Fire", line1[3], true)
-              .addField("Water", line1[4], true)
-              .addField("Wind", line1[5], true)
-              .addField("Light", line1[6], true)
-              .addField("Dark", line1[7], true)
-              
-            pages.push(embed);
-          }
-          if (check) {
-            functions.sende(message, pages);
-          }
-          if (!check) {
-            message.channel.send("No Data");
-          }
-        } else {
-          message.channel.send("No Data");
+        var tk = $('.categories').text().includes("Train Knights");
+				if (tk) {
+          for (var i = 1; i < $(".wikitable tbody").eq(0).children().length; i++) {
+            let img = $(".wikitable tbody tr:eq("+i+") td:eq(0) a img").attr('src')
+            let link = $(".wikitable tbody tr:eq("+i+") td:eq(1) a").attr('href')
+            pages = find(pages, link, img)
+            }
         }
       }
-    });
+    })
   }
+}
+
+function find(pages, link, img) {
+  var pages2 = pages;
+  request(link, function (err, resp, html) {
+    if (!err) {
+      const $ = cheerio.load(html);
+      var output;
+      var aff;
+      var check = false;
+
+      output = $(".wikitable.hidden.stats tr:nth-child(4)").html();
+      if (output) {
+        check = true;
+        let line1 = lv1line(
+          $(".wikitable.hidden.stats tr:nth-child(3)").html()
+        );
+        let line2 = lv1line(
+          $(".wikitable.hidden.stats tr:nth-child(4)").html()
+        );
+        let embed = new Discord.RichEmbed()
+          .setURL(link)
+          .setTitle(unit + "'s Stats")
+          .setThumbnail(img)
+          .setColor("LIGHT_GREY")
+          .addField("ATK", line1[1] + " → " + line2[1], true)
+          .addField("DEF", line1[2] + " → " + line2[2], true)
+          .addField("HIT", line1[3] + " → " + line2[3], true)
+          .addField("SPD", line1[4] + " → " + line2[4], true)
+          .addField("MATK", line1[5] + " → " + line2[5], true)
+          .addField("MDEF", line1[6] + " → " + line2[6], true)
+          .addField("RCV", line1[7] + " → " + line2[7], true)
+          .addField("LUK", line1[8] + " → " + line2[8], true);
+        pages2.push(embed);
+      }
+      output = $(".wikitable.hidden.resist tr:nth-child(2)").html();
+      if (output) {
+        check = true;
+        let line1 = lv1line(
+          $(".wikitable.hidden.resist tr:nth-child(2)").html()
+        );
+        let embed = new Discord.RichEmbed()
+          .setURL(link)
+          .setTitle(unit + "'s Resists")
+          .setThumbnail(img)
+          .setColor("LIGHT_GREY")
+          .addField("Cut", line1[0], true)
+          .addField("Blow", line1[1], true)
+          .addField("Pierce", line1[2], true)
+          .addField("Fire", line1[3], true)
+          .addField("Water", line1[4], true)
+          .addField("Wind", line1[5], true)
+          .addField("Light", line1[6], true)
+          .addField("Dark", line1[7], true);
+
+        pages2.push(embed);
+      }
+    }
+  });
+  return pages2
 }
 
 function lv1line(output) {
